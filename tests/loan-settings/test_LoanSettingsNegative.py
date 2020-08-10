fbc09bf528da34335e590ffef5d2809e1efe8f92
Roman Iftodi <romeo8881@gmail.com>
2020-08-10 20:01:33 +0300

    removed fallback funciton test

--- tests/loan-settings/test_LoanSettingsNegative.py
+++ tests/loan-settings/test_LoanSettingsNegative.py
@@ -31,16 +31,9 @@ def test_disableUnauthorizedOwnerLoanSettings(bzx, accounts, DAI, loanParamsId,)
     with reverts("unauthorized owner"):
         bzx.disableLoanParams([loanParamsId], { "from": accounts[1] })
 
-# TODO @Tom help: is there a way to get LoanSettings address
-# def test_LoanSettings_fallback_not_allowd(bzx, accounts, DAI, web3, bZxProtocol):
-#     print("LoanSettings", bZxProtocol)
-#     with reverts("unauthorized owner"):
-        # web3.eth.sendTransaction({ "from": str(accounts[0]), "to": bZxProtocol.address, "value": 1, "gas": "5999" })
-
 def test_LoanSettings_loanParamAlreadyExists(bzx, accounts, DAI, loanParamsId, loanParams):
     with reverts("loanParams exists"):
         bzx.setupLoanParams([list(loanParams.values()), list(loanParams.values())])
-
 
 def test_LoanSettings_otherRequires(bzx, accounts, DAI, loanParamsId, loanParams, Constants):
 
