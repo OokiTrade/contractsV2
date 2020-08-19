c95050eb5ee1f6e0dbf7869fe391306572011541
Roman Iftodi <roman.iftodi.me@gmail.com>
2020-08-19 10:24:05 +0300

    placeholder message

--- tests/loan-closing/test_LoanClosing.py
+++ tests/loan-closing/test_LoanClosing.py
@@ -3,16 +3,4 @@
 # import pytest
 # from brownie import Wei, reverts
 
-
-# def test_liquidateWithGasToken(bzx):
-#     assert False
-
-# def test_rolloverWithGasToken(bzx):
-#     assert False
-
-# def test_closeWithDepositWithGasToken(bzx):
-#     assert False
-
-# def test_closeWithSwapWithGasToken(bzx):
-#     assert False
-
+# more complex scenario here in the future
