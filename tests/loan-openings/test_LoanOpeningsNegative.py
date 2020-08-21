30e77f5e09e774730935ab4c89feed701c858e93
Roman Iftodi <roman.iftodi.me@gmail.com>
2020-08-21 10:03:17 +0300

    tests so far

--- tests/loan-openings/test_LoanOpeningsNegative.py
+++ tests/loan-openings/test_LoanOpeningsNegative.py
@@ -4,33 +4,101 @@ import pytest
 from brownie import Wei, reverts
 from helpers import getLoanId
 
-# def test_borrowOrTradeFromPoolLoanDataBytesRequiredWithEther(Constants, bzx):
-#     with reverts("loanDataBytes required with ether"):
-#         bzx.borrowOrTradeFromPool(0, 0)
+@pytest.fixture(scope="module")
+def setLoanPool(bzx, accounts):
+    return bzx.setLoanPool(
+        [
+            accounts[1],
+        ],
+        [
+            accounts[2]
+        ]
+    )
 
-# def test_borrowOrTradeFromPoolNotAuthorized(Constants, bzx):
-#     with reverts("not authorized"):
-#         bzx.borrowOrTradeFromPool(0, 0)
+@pytest.fixture(scope="module")
+def linkDaiMarginParamsId(Constants, LINK, DAI, bzx, accounts):
 
-# def test_borrowOrTradeFromPoolLoanParamsNotExist(Constants, bzx):
-#     with reverts("loanParams not exists"):
-#         bzx.borrowOrTradeFromPool(0, 0)
+    loanParams = {
+        "id": "0x0",
+        "active": False,
+        "owner": Constants["ZERO_ADDRESS"],
+        "loanToken": DAI.address,
+        "collateralToken": LINK.address,
+        "minInitialMargin": 20e18,
+        "maintenanceMargin": 15e18,
+        "fixedLoanTerm": "2419200" # 28 days
+    }
+    tx = bzx.setupLoanParams([list(loanParams.values())])
+    return tx.events["LoanParamsIdSetup"][0]["id"]
 
-# def test_borrowOrTradeFromPoolCollateralIsZero(Constants, bzx):
-#     with reverts("collateral is 0"):
-#         bzx.borrowOrTradeFromPool(0, 0)
+@pytest.fixture(scope="module")
+def sameTokenParamsId(Constants, LINK, DAI, bzx, accounts):
 
-# def test_setDelegatedManagerUnauthorized(Constants, bzx):
-#     with reverts("unauthorized"):
-#         bzx.borrowOrTradeFromPool(0, 0)
+    loanParams = {
+        "id": "0x0",
+        "active": False,
+        "owner": Constants["ZERO_ADDRESS"],
+        "loanToken": DAI.address,
+        "collateralToken": DAI.address,
+        "minInitialMargin": 20e18,
+        "maintenanceMargin": 15e18,
+        "fixedLoanTerm": "2419200" # 28 days
+    }
+    tx = bzx.setupLoanParams([list(loanParams.values())])
+    return tx.events["LoanParamsIdSetup"][0]["id"]
 
-# def test_borrowOrTradeFromPoolCollateralLoanMatch(Constants, bzx):
-#     with reverts("collateral/loan match"):
-#         bzx.borrowOrTradeFromPool(0, 0)
+def test_borrowOrTradeFromPoolLoanDataBytesRequiredWithEther(Constants, bzx):
+    with reverts("loanDataBytes required with ether"):
+        tx = bzx.borrowOrTradeFromPool(0, 0, 0, 0, 
+        [Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"]], 
+        [0, 0, 0, 0, 0], 
+        b"", {"value": "1 ether"})
+        tx.info()
 
-# def test_borrowOrTradeFromPoolInitialMarginTooLow(Constants, bzx):
+def test_borrowOrTradeFromPoolNotAuthorized(Constants, bzx):
+    with reverts("not authorized"):
+        tx = bzx.borrowOrTradeFromPool(0, 0, 0, 0, 
+            [Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"]], 
+            [0, 0, 0, 0, 0], 
+            b"", {"value": "0 ether"})
+        tx.info()
+
+def test_borrowOrTradeFromPoolLoanParamsNotExist(Constants, bzx, setLoanPool, accounts):
+    with reverts("loanParams not exists"):
+        tx = bzx.borrowOrTradeFromPool(0, 0, 0, 0, 
+            [Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"]], 
+            [0, 0, 0, 0, 0], 
+            b"", {"from": accounts[1], "value": "0 ether"})
+        tx.info()
+
+def test_borrowOrTradeFromPoolCollateralIsZero(Constants, bzx, linkDaiMarginParamsId, accounts):
+    with reverts("collateral is 0"):
+        tx = bzx.borrowOrTradeFromPool(linkDaiMarginParamsId, 0, 0, 0, 
+            [Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"]], 
+            [0, 0, 0, 0, 0], 
+            b"", {"from": accounts[1], "value": "0 ether"})
+        tx.info()
+
+
+def test_borrowOrTradeFromPoolCollateralLoanMatch(Constants, bzx, sameTokenParamsId, accounts):
+    initialMargin = 10**20
+    newPrincipal = 1
+    with reverts("collateral/loan match"):
+        tx = bzx.borrowOrTradeFromPool(sameTokenParamsId, 1, False, initialMargin, 
+            [Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"]], 
+            [1, newPrincipal, 1, 1, 1], 
+            b"", {"from": accounts[1], "value": "0 ether"})
+        tx.info()
+
+# def test_borrowOrTradeFromPoolInitialMarginTooLow(Constants, bzx, linkDaiMarginParamsId, accounts):
+#     initialMargin = 10**20
+#     newPrincipal = 1
 #     with reverts("initialMargin too low"):
-#         bzx.borrowOrTradeFromPool(0, 0)
+#         tx = bzx.borrowOrTradeFromPool(linkDaiMarginParamsId, 1, False, initialMargin, 
+#             [Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"], Constants["ZERO_ADDRESS"]], 
+#             [1, newPrincipal, 1, 1, 1], 
+#             b"", {"from": accounts[1], "value": "0 ether"})
+#         tx.info()
 
 # def test_borrowOrTradeFromPoolInvalidInterest(Constants, bzx):
 #     with reverts("invalid interest"):
@@ -67,3 +135,7 @@ from helpers import getLoanId
 # def test_borrowOrTradeFromPoolCollateralInsuficient(Constants, bzx):
 #     with reverts("collateral insufficient"):
 #         bzx.borrowOrTradeFromPool(0, 0)
+
+# def test_setDelegatedManagerUnauthorized(Constants, bzx):
+#     with reverts("unauthorized"):
+#         bzx.setDelegatedManager(0, Constants["ZERO_ADDRESS"], 0);
