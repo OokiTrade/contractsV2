d9190e5ab52817f98a0e6364e7c51e354ca97c8c
Tom Bean <tomcbean@gmail.com>
2020-08-29 20:07:06 -0400

    optimizations

--- contracts/modules/LoanMaintenance/LoanMaintenance.sol
+++ contracts/modules/LoanMaintenance/LoanMaintenance.sol
@@ -453,11 +453,6 @@ contract LoanMaintenance is State, LoanMaintenanceEvents, VaultController, Inter
         LoanReturnData memory loanData;
         loansData = new LoanReturnData[](idx);
         for (uint256 i = --end; i >= start; i--) {
-            if (i > end) {
-                // handles the overflow in the case of start == 0
-                break;
-            }
-
             loanData = _getLoan(
                 set.get(i), // loanId
                 loanType,
@@ -472,6 +467,10 @@ contract LoanMaintenance is State, LoanMaintenanceEvents, VaultController, Inter
             }
 
             loansData[count-(idx--)] = loanData;
+
+            if (i == 0) {
+                break;
+            }
         }
 
         if (idx != 0) {
@@ -513,11 +512,6 @@ contract LoanMaintenance is State, LoanMaintenanceEvents, VaultController, Inter
         LoanReturnData memory loanData;
         loansData = new LoanReturnData[](idx);
         for (uint256 i = --end; i >= start; i--) {
-            if (i > end) {
-                // handles the overflow in the case of start == 0
-                break;
-            }
-
             loanData = _getLoan(
                 activeLoansSet.get(i), // loanId
                 LoanType.All,
@@ -532,6 +526,10 @@ contract LoanMaintenance is State, LoanMaintenanceEvents, VaultController, Inter
             }
 
             loansData[count-(idx--)] = loanData;
+
+            if (i == 0) {
+                break;
+            }
         }
 
         if (idx != 0) {
