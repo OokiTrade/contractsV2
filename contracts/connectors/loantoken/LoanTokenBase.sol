292d146a6525a0aebe231db5ac40a15e3339a389
Roman Iftodi <roman.iftodi.me@gmail.com>
2020-09-15 15:26:34 +0300

    removed init price

--- contracts/connectors/loantoken/LoanTokenBase.sol
+++ contracts/connectors/loantoken/LoanTokenBase.sol
@@ -41,7 +41,6 @@ contract LoanTokenBase is ReentrancyGuard, Ownable, Pausable {
 
     uint256 internal _flTotalAssetSupply;
     uint256 public checkpointSupply;
-    uint256 public initialPrice;
 
     mapping (uint256 => bytes32) public loanParamsIds; // mapping of keccak256(collateralToken, isTorqueLoan) to loanParamsId
     mapping (address => uint256) internal checkpointPrices_; // price of token at last user checkpoint
