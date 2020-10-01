dd1a1898a1a28daea1f2106aa56e447b926e3bb9
Roman Iftodi <romeo8881@gmail.com>
2020-10-01 21:25:26 +0300

    formated

--- tests/loan-openings/test_LoanOpeningsEvents.py
+++ tests/loan-openings/test_LoanOpeningsEvents.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 import pytest
-from brownie import Wei, reverts 
+from brownie import Wei, reverts
 from helpers import getLoanId, setupLoanPool
+
 
 @pytest.fixture(scope="module")
 def LinkDaiBorrowParamsId(Constants, LINK, DAI, bzx, accounts, WETH):
@@ -14,10 +15,11 @@ def LinkDaiBorrowParamsId(Constants, LINK, DAI, bzx, accounts, WETH):
         "collateralToken": LINK.address,
         "minInitialMargin": 20e18,
         "maintenanceMargin": 15e18,
-        "fixedLoanTerm": "0", # torque loan
+        "fixedLoanTerm": "0",  # torque loan
     }
     tx = bzx.setupLoanParams([list(loanParams.values())])
     return tx.events["LoanParamsIdSetup"][0]["id"]
+
 
 @pytest.fixture(scope="module")
 def LinkDaiTradeParamsId(Constants, LINK, DAI, bzx, accounts, WETH):
@@ -29,29 +31,16 @@ def LinkDaiTradeParamsId(Constants, LINK, DAI, bzx, accounts, WETH):
         "collateralToken": LINK.address,
         "minInitialMargin": 20e18,
         "maintenanceMargin": 15e18,
-        "fixedLoanTerm": "2419200" # 28 days
+        "fixedLoanTerm": "2419200"  # 28 days
     }
     tx = bzx.setupLoanParams([list(loanParams.values())])
     return tx.events["LoanParamsIdSetup"][0]["id"]
 
-@pytest.fixture(scope="module")
-def linkDaiMarginParamsId(Constants, LINK, DAI, bzx, accounts):
-    loanParams = {
-        "id": "0x0",
-        "active": False,
-        "owner": Constants["ZERO_ADDRESS"],
-        "loanToken": DAI.address,
-        "collateralToken": LINK.address,
-        "minInitialMargin": 20e18,
-        "maintenanceMargin": 15e18,
-        "fixedLoanTerm": "2419200" # 28 days
-    }
-    tx = bzx.setupLoanParams([list(loanParams.values())])
-    return tx.events["LoanParamsIdSetup"][0]["id"]
 
 @pytest.fixture(scope="module")
 def loanId_LINK_DAI(Constants, bzx, DAI, LINK, accounts, web3, LinkDaiBorrowParamsId):
     return getLoanId(Constants, bzx, DAI, LINK, accounts, web3, LinkDaiBorrowParamsId)
+
 
 @pytest.fixture(scope="module")
 def setup(Constants, bzx, LINK, DAI, accounts):
@@ -63,7 +52,7 @@ def setup(Constants, bzx, LINK, DAI, accounts):
     DAI.mint(
         bzx.address,
         loanTokenSent,
-        { "from": accounts[0] }
+        {"from": accounts[0]}
     )
 
     collateralTokenSent = bzx.getRequiredCollateral(
@@ -77,74 +66,78 @@ def setup(Constants, bzx, LINK, DAI, accounts):
     LINK.mint(
         bzx.address,
         collateralTokenSent,
-        { "from": accounts[0] }
+        {"from": accounts[0]}
     )
     return collateralTokenSent
 
+
 def test_borrowOrTradeFromPoolBorrowEvent(Constants, bzx, accounts, LinkDaiBorrowParamsId, DAI, LINK, setup):
     tx = bzx.borrowOrTradeFromPool(
-        LinkDaiBorrowParamsId, #loanParamsId
-        "0", # loanId - starts a new loan
-        True, # isTorqueLoan,
-        50e18, # initialMargin
+        LinkDaiBorrowParamsId,  # loanParamsId
+        "0",  # loanId - starts a new loan
+        True,  # isTorqueLoan,
+        50e18,  # initialMargin
         [
-            accounts[2], # lender
-            accounts[1], # borrower
-            accounts[1], # receiver
-            Constants["ZERO_ADDRESS"], # manager
+            accounts[2],  # lender
+            accounts[1],  # borrower
+            accounts[1],  # receiver
+            Constants["ZERO_ADDRESS"],  # manager
         ],
         [
-            5e18, # newRate (5%)
-            101e18, # newPrincipal
-            1e18, # torqueInterest
-            1e18, # loanTokenSent
-            setup # collateralTokenSent
+            5e18,  # newRate (5%)
+            101e18,  # newPrincipal
+            1e18,  # torqueInterest
+            1e18,  # loanTokenSent
+            setup  # collateralTokenSent
         ],
-        b'', # loanDataBytes
-        { "from": accounts[1] }
+        b'',  # loanDataBytes
+        {"from": accounts[1]}
     )
 
     tx.info()
 
     borrowEvent = tx.events["Borrow"][0]
     assert(borrowEvent["user"] == accounts[1])
-    assert(borrowEvent["lender"] == accounts[2]) 
+    assert(borrowEvent["lender"] == accounts[2])
     assert(borrowEvent["loanToken"] == DAI)
     assert(borrowEvent["collateralToken"] == LINK)
-    assert(borrowEvent["newPrincipal"] == 101e18) 
+    assert(borrowEvent["newPrincipal"] == 101e18)
+
 
 def test_borrowOrTradeFromPoolTradeEvent(Constants, bzx, accounts, LinkDaiTradeParamsId, DAI, LINK, setup):
     tx = bzx.borrowOrTradeFromPool(
-        LinkDaiTradeParamsId, #loanParamsId
-        "0", # loanId - starts a new loan
-        False, # isTorqueLoan,
-        50e18, # initialMargin
+        LinkDaiTradeParamsId,  # loanParamsId
+        "0",  # loanId - starts a new loan
+        False,  # isTorqueLoan,
+        50e18,  # initialMargin
         [
-            accounts[2], # lender
-            accounts[1], # borrower
-            accounts[1], # receiver
-            Constants["ZERO_ADDRESS"], # manager
+            accounts[2],  # lender
+            accounts[1],  # borrower
+            accounts[1],  # receiver
+            Constants["ZERO_ADDRESS"],  # manager
         ],
         [
-            5e18, # newRate (5%)
-            101e18, # newPrincipal
-            0, # torqueInterest
-            1e18, # loanTokenSent
-            setup # collateralTokenSent
+            5e18,  # newRate (5%)
+            101e18,  # newPrincipal
+            0,  # torqueInterest
+            1e18,  # loanTokenSent
+            setup  # collateralTokenSent
         ],
-        b'', # loanDataBytes
-        { "from": accounts[1] }
+        b'',  # loanDataBytes
+        {"from": accounts[1]}
     )
     tx.info()
 
     borrowEvent = tx.events["Trade"][0]
     assert(borrowEvent["user"] == accounts[1])
-    assert(borrowEvent["lender"] == accounts[2]) 
+    assert(borrowEvent["lender"] == accounts[2])
     assert(borrowEvent["loanToken"] == DAI)
     assert(borrowEvent["collateralToken"] == LINK)
 
+
 def test_setDelegatedManagerSetEvent(Constants, bzx, accounts, loanId_LINK_DAI):
-    tx = bzx.setDelegatedManager(loanId_LINK_DAI, accounts[2], True, {"from": accounts[1]})
+    tx = bzx.setDelegatedManager(
+        loanId_LINK_DAI, accounts[2], True, {"from": accounts[1]})
     tx.info()
     delegatedManagerSet = tx.events["DelegatedManagerSet"][0]
     assert(delegatedManagerSet["loanId"] == loanId_LINK_DAI)
@@ -161,7 +154,7 @@ def test_EarnRewardEvent(Constants, bzx, accounts, LinkDaiTradeParamsId, LINK, D
     DAI.mint(
         bzx.address,
         loanTokenSent,
-        { "from": accounts[0] }
+        {"from": accounts[0]}
     )
     collateralTokenSent = bzx.getRequiredCollateral(
         DAI.address,
@@ -173,29 +166,29 @@ def test_EarnRewardEvent(Constants, bzx, accounts, LinkDaiTradeParamsId, LINK, D
     LINK.mint(
         bzx.address,
         collateralTokenSent,
-        { "from": accounts[0] }
+        {"from": accounts[0]}
     )
 
     tx = bzx.borrowOrTradeFromPool(
-        LinkDaiTradeParamsId, #loanParamsId
-        "0", # loanId
-        False, # isTorqueLoan,
-        100e18, # initialMargin
+        LinkDaiTradeParamsId,  # loanParamsId
+        "0",  # loanId
+        False,  # isTorqueLoan,
+        100e18,  # initialMargin
         [
-            accounts[2], # lender
-            accounts[1], # borrower
-            accounts[1], # receiver
-            Constants["ZERO_ADDRESS"], # manager
+            accounts[2],  # lender
+            accounts[1],  # borrower
+            accounts[1],  # receiver
+            Constants["ZERO_ADDRESS"],  # manager
         ],
         [
-            5e18, # newRate (5%)
-            loanTokenSent, # newPrincipal
-            0, # torqueInterest
-            loanTokenSent, # loanTokenSent
-            collateralTokenSent # collateralTokenSent
+            5e18,  # newRate (5%)
+            loanTokenSent,  # newPrincipal
+            0,  # torqueInterest
+            loanTokenSent,  # loanTokenSent
+            collateralTokenSent  # collateralTokenSent
         ],
-        b'', # loanDataBytes
-        { "from": accounts[1] }
+        b'',  # loanDataBytes
+        {"from": accounts[1]}
     )
     tradeEvent = tx.events['Trade'][0]
     assert(tradeEvent['user'] == accounts[1])
@@ -206,7 +199,10 @@ def test_EarnRewardEvent(Constants, bzx, accounts, LinkDaiTradeParamsId, LINK, D
     loanId = tradeEvent['loanId']
 
     payTradingFeeEvent = tx.events[0][0]
-    assert(payTradingFeeEvent['topic1'] == '0xb23479169712c443e6b00fb0cec3506a5f5926f541df4243d313e11c8c5c71ed') # kessak of PayTradinfFee event
-    assert('0X'+ payTradingFeeEvent['topic2'][26:66] == accounts[1])
-    assert(('0X'+ payTradingFeeEvent['topic3'][26:66].upper()) == DAI.address.upper())
+    # kessak of PayTradinfFee event
+    assert(payTradingFeeEvent['topic1'] ==
+           '0xb23479169712c443e6b00fb0cec3506a5f5926f541df4243d313e11c8c5c71ed')
+    assert('0X' + payTradingFeeEvent['topic2'][26:66] == accounts[1])
+    assert(('0X' + payTradingFeeEvent['topic3']
+            [26:66].upper()) == DAI.address.upper())
     assert(payTradingFeeEvent['topic4'] == loanId)
