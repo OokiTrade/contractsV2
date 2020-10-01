65f81f8c1264df39388770d8448a4c00eaf61af1
Roman Iftodi <romeo8881@gmail.com>
2020-10-01 21:24:27 +0300

    EarnReward and PayTradingFee events were missing loanId

--- tests/loan-openings/test_LoanOpeningsEvents.py
+++ tests/loan-openings/test_LoanOpeningsEvents.py
@@ -35,6 +35,21 @@ def LinkDaiTradeParamsId(Constants, LINK, DAI, bzx, accounts, WETH):
     return tx.events["LoanParamsIdSetup"][0]["id"]
 
 @pytest.fixture(scope="module")
+def linkDaiMarginParamsId(Constants, LINK, DAI, bzx, accounts):
+    loanParams = {
+        "id": "0x0",
+        "active": False,
+        "owner": Constants["ZERO_ADDRESS"],
+        "loanToken": DAI.address,
+        "collateralToken": LINK.address,
+        "minInitialMargin": 20e18,
+        "maintenanceMargin": 15e18,
+        "fixedLoanTerm": "2419200" # 28 days
+    }
+    tx = bzx.setupLoanParams([list(loanParams.values())])
+    return tx.events["LoanParamsIdSetup"][0]["id"]
+
+@pytest.fixture(scope="module")
 def loanId_LINK_DAI(Constants, bzx, DAI, LINK, accounts, web3, LinkDaiBorrowParamsId):
     return getLoanId(Constants, bzx, DAI, LINK, accounts, web3, LinkDaiBorrowParamsId)
 
@@ -136,3 +151,62 @@ def test_setDelegatedManagerSetEvent(Constants, bzx, accounts, loanId_LINK_DAI):
     assert(delegatedManagerSet["delegator"] == accounts[1])
     assert(delegatedManagerSet["delegated"] == accounts[2])
     assert(delegatedManagerSet["isActive"])
+
+
+def test_EarnRewardEvent(Constants, bzx, accounts, LinkDaiTradeParamsId, LINK, DAI):
+
+    setupLoanPool(Constants, bzx, accounts[1], accounts[2])
+    loanTokenSent = 100e18
+
+    DAI.mint(
+        bzx.address,
+        loanTokenSent,
+        { "from": accounts[0] }
+    )
+    collateralTokenSent = bzx.getRequiredCollateral(
+        DAI.address,
+        LINK.address,
+        loanTokenSent,
+        100e18,
+        False
+    )
+    LINK.mint(
+        bzx.address,
+        collateralTokenSent,
+        { "from": accounts[0] }
+    )
+
+    tx = bzx.borrowOrTradeFromPool(
+        LinkDaiTradeParamsId, #loanParamsId
+        "0", # loanId
+        False, # isTorqueLoan,
+        100e18, # initialMargin
+        [
+            accounts[2], # lender
+            accounts[1], # borrower
+            accounts[1], # receiver
+            Constants["ZERO_ADDRESS"], # manager
+        ],
+        [
+            5e18, # newRate (5%)
+            loanTokenSent, # newPrincipal
+            0, # torqueInterest
+            loanTokenSent, # loanTokenSent
+            collateralTokenSent # collateralTokenSent
+        ],
+        b'', # loanDataBytes
+        { "from": accounts[1] }
+    )
+    tradeEvent = tx.events['Trade'][0]
+    assert(tradeEvent['user'] == accounts[1])
+    assert(tradeEvent['lender'] == accounts[2])
+    assert(tradeEvent['collateralToken'] == LINK.address)
+    assert(tradeEvent['loanToken'] == DAI.address)
+
+    loanId = tradeEvent['loanId']
+
+    payTradingFeeEvent = tx.events[0][0]
+    assert(payTradingFeeEvent['topic1'] == '0xb23479169712c443e6b00fb0cec3506a5f5926f541df4243d313e11c8c5c71ed') # kessak of PayTradinfFee event
+    assert('0X'+ payTradingFeeEvent['topic2'][26:66] == accounts[1])
+    assert(('0X'+ payTradingFeeEvent['topic3'][26:66].upper()) == DAI.address.upper())
+    assert(payTradingFeeEvent['topic4'] == loanId)
