6853f9bf86303393602d431cfbd4e2862ce8b579
Tom Bean <tomcbean@gmail.com>
2020-12-03 23:08:45 -0500

    update to payout

--- scripts/rewards.py
+++ scripts/rewards.py
@@ -175,5 +175,5 @@ for row in overallResults:
 
 print("results:")
 for row in overallResults:
-    # print(row[0], '{0:.4f}'.format(row[3] * 2060000), '{0:.0f}'.format(row[3] * 2060000 * 1000000000000000000))
-    print('0x'+row[0][24:], '{0:.4f}'.format(row[3] * 2060000), '{0:.0f}'.format(row[3] * 2060000 * 1000000000000000000))
+    # print(row[0], '{0:.4f}'.format(row[3] * 1545000), '{0:.0f}'.format(row[3] * 1545000 * 1000000000000000000))
+    print('0x'+row[0][24:], '{0:.4f}'.format(row[3] * 1545000), '{0:.0f}'.format(row[3] * 1545000 * 1000000000000000000))
