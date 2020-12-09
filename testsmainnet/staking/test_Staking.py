4046a8196b162864a5f744b7037514fb371ffd4a
Roman Iftodi <romeo8881@gmail.com>
2020-12-09 17:57:50 +0200

    more tests

--- testsmainnet/staking/test_Staking.py
+++ testsmainnet/staking/test_Staking.py
@@ -10,8 +10,22 @@ def requireMainnetFork():
 
 
 @pytest.fixture(scope="module")
-def setFeesController(bzx, stakingV1):
+def setFeesController(bzx, stakingV1, accounts):
     bzx.setFeesController(stakingV1, {"from": bzx.owner()})
+    assets = [
+        "0x56d811088235F11C8920698a204A5010a788f4b3",  # BZRX
+        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",  # WETH
+        "0x6B175474E89094C44Da98b954EedeAC495271d0F",  # DAI
+        "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",  # USDC
+        "0xdAC17F958D2ee523a2206206994597C13D831ec7",  # USDT
+        "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",  # WBTC
+        "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",  # AAVE
+        "0xdd974D5C2e2928deA5F71b9825b8b646686BD200",  # KNC
+        "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2",  # MKR
+        "0x514910771AF9Ca656af840dff83E8264EcF986CA",  # LINK
+        "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e",  # YFI
+    ]
+    bzx.withdrawFees(assets, accounts[8], 0, {'from': stakingV1})
 
 
 @pytest.fixture(scope="module")
@@ -201,21 +215,16 @@ def testStake_SweeepFees(requireMainnetFork, stakingV1, bzx, setFeesController, 
 
 
 def testStake_BZRXProfit(requireMainnetFork, stakingV1, bzx, setFeesController, BZRX, vBZRX, iBZRX, accounts, iUSDC, USDC, WETH):
-    assert False
-    stakingV1.sweepFees()
 
     earnedAmounts = stakingV1.earned(accounts[0])
     assert(earnedAmounts == (0, 0))
     print("earnedAmounts", earnedAmounts)
     balanceOfBZRX = BZRX.balanceOf(accounts[0])
-    balanceOfvBZRX = vBZRX.balanceOf(accounts[0])
-    balanceOfiBZRX = iBZRX.balanceOf(accounts[0])
 
     BZRX.approve(stakingV1, balanceOfBZRX, {'from': accounts[0]})
-    vBZRX.approve(stakingV1, balanceOfvBZRX, {'from': accounts[0]})
-    iBZRX.approve(stakingV1, balanceOfiBZRX, {'from': accounts[0]})
+
     tokens = [BZRX, vBZRX, iBZRX]
-    amounts = [balanceOfBZRX, balanceOfvBZRX, balanceOfiBZRX]
+    amounts = [balanceOfBZRX, 0, 0]
     tx = stakingV1.stakeWithDelegate(tokens, amounts, accounts[1])
 
     # iUSDC.borrow(0, 100*10**18, 1*10**18, "0x0000000000000000000000000000000000000000", accounts[0], accounts[0], {'value': Wei("1 ether")})
@@ -226,24 +235,6 @@ def testStake_BZRXProfit(requireMainnetFork, stakingV1, bzx, setFeesController, 
     txBorrow = iUSDC.borrow("", borrowAmount, borrowTime, collateralAmount, collateralAddress,
                             accounts[0], accounts[0], b"", {'from': accounts[0], 'value': Wei(collateralAmount)})
 
-    # assets = [
-    #     "0x56d811088235F11C8920698a204A5010a788f4b3", # BZRX
-    #     "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", # WETH
-    #     "0x6B175474E89094C44Da98b954EedeAC495271d0F", # DAI
-    #     "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", # USDC
-    #     # "0xdAC17F958D2ee523a2206206994597C13D831ec7", # USDT TODO issue @Tom
-    #     "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599", # WBTC
-    #     "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", # AAVE
-    #     "0xdd974D5C2e2928deA5F71b9825b8b646686BD200", # KNC
-    #     "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2", # MKR
-    #     "0x514910771AF9Ca656af840dff83E8264EcF986CA", # LINK
-    #     "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e", # YFI
-    # ]
-
-    # for address in assets:
-    #     print("address", address)
-    #     stakingV1.sweepFeesByAsset([address])
-
     payBorrowingFeeEvent = filterEvents(
         '0xfb6c38ae4fdd498b3a5003f02ca4ca5340dfedb36b1b100c679eb60633b2c0a7', txBorrow.events)
     payBorrowingFeeAmount = int(str(payBorrowingFeeEvent['data']), 0)
@@ -252,7 +243,6 @@ def testStake_BZRXProfit(requireMainnetFork, stakingV1, bzx, setFeesController, 
         '0x40a75ae5f7a5336e75f7c7977e12c4b46a9ac0f30de01a2d5b6c1a4f4af63587', txBorrow.events)
     payLendingFeeAmount = int(str(payLendingFeeEvent['data']), 0)
 
-    assert False
     txSweep = stakingV1.sweepFees()
 
     borrowFee = txSweep.events['WithdrawBorrowingFees'][0]
@@ -260,18 +250,51 @@ def testStake_BZRXProfit(requireMainnetFork, stakingV1, bzx, setFeesController, 
     assert(borrowFee['token'], WETH)
     assert(borrowFee['sender'], stakingV1)
     assert(borrowFee['amount'], payBorrowingFeeAmount)
- 
+
     lendingFee = txSweep.events['WithdrawLendingFees'][0]
     assert(lendingFee['sender'], stakingV1)
     assert(lendingFee['token'], USDC)
     assert(lendingFee['sender'], stakingV1)
     assert(lendingFee['amount'], payLendingFeeAmount)
 
-    assert False
-    bzrxRewards = txSweep.events['RewardAdded'][0]['bzrxAmount']
+    assert(txSweep.events['RewardAdded'][0]['sender'] == accounts[0])
+    bzrxAmount = txSweep.events['RewardAdded'][0]['bzrxAmount']
     stableCoinAmount = txSweep.events['RewardAdded'][0]['stableCoinAmount']
 
+    assert(txSweep.events['DistributeFees'][0]['sender'] == accounts[0])
+    bzrxRewards = txSweep.events['DistributeFees'][0]['bzrxRewards']
+    stableCoinRewards = txSweep.events['DistributeFees'][0]['stableCoinRewards']
+
+    assert(bzrxAmount == bzrxRewards)
+    assert(stableCoinAmount == stableCoinRewards)
     earned = stakingV1.earned(accounts[0])
+
+    # we have roundings for last 3 digits
+    assert(bzrxRewards - earned[0] < 1000)
+    # we have roundings for last 3 digits
+    assert(stableCoinAmount - earned[1] < 1000)
+
+    # second user staking. he should get zero rewards if he just staked
+    earnedAmounts = stakingV1.earned(accounts[1])
+    assert(earnedAmounts == (0, 0))
+    BZRX.transfer(accounts[1], 1000*10**18, {'from': BZRX.address})
+
+    balanceOfBZRX = BZRX.balanceOf(accounts[1])
+
+    BZRX.approve(stakingV1, balanceOfBZRX, {'from': accounts[1]})
+
+    tokens = [BZRX, vBZRX, iBZRX]
+    amounts = [balanceOfBZRX, 0, 0]
+    tx = stakingV1.stakeWithDelegate(
+        tokens, amounts, accounts[1], {'from': accounts[1]})
+
+    earnedAmounts = stakingV1.earned(accounts[1])
+    assert(earnedAmounts == (0, 0))
+
+    txBorrow = iUSDC.borrow("", borrowAmount, borrowTime, collateralAmount, collateralAddress,
+                            accounts[0], accounts[0], b"", {'from': accounts[0], 'value': Wei(collateralAmount)})
+
+    txSweepSecondAcc = stakingV1.sweepFees()
 
     assert False
 
