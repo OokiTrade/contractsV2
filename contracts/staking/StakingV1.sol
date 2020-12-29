21144fc43dc8e1b320e14b1bae5f6efbac37c44d
Tom Bean <tomcbean@gmail.com>
2020-12-29 00:24:56 -0500

    small updates

--- contracts/staking/StakingV1.sol
+++ contracts/staking/StakingV1.sol
@@ -200,7 +200,7 @@ contract StakingV1 is StakingState, StakingConstants {
 
     function claimWithUpdate()
         external
-        // sweepFees() does checkPause
+        // sweepFeesByAsset() does checkPause
         returns (uint256 bzrxRewardsEarned, uint256 stableCoinRewardsEarned)
     {
         sweepFees();
@@ -209,7 +209,7 @@ contract StakingV1 is StakingState, StakingConstants {
 
     function claimAndRestakeWithUpdate()
         external
-        // sweepFees() does checkPause
+        // sweepFeesByAsset() does checkPause
         returns (uint256 bzrxRewardsEarned, uint256 stableCoinRewardsEarned)
     {
         sweepFees();
@@ -293,7 +293,7 @@ contract StakingV1 is StakingState, StakingConstants {
 
     function exitWithUpdate()
         external
-        // sweepFees() does checkPause
+        // sweepFeesByAsset() does checkPause
     {
         sweepFees();
         exit();
@@ -391,7 +391,7 @@ contract StakingV1 is StakingState, StakingConstants {
     function earnedWithUpdate(
         address account)
         external
-        // sweepFees() does checkPause
+        // sweepFeesByAsset() does checkPause
         returns (uint256, uint256, uint256, uint256) // bzrxRewardsEarned, stableCoinRewardsEarned, bzrxRewardsVesting, stableCoinRewardsVesting
     {
         sweepFees();
@@ -603,7 +603,7 @@ contract StakingV1 is StakingState, StakingConstants {
 
             uint256 _lastRewardsAddTime = lastRewardsAddTime;
             if (_lastRewardsAddTime != 0) {
-                // user is attributed to a staked balance of vested BZRX up to the time rewards were last added
+                // user is attributed a staked balance of vested BZRX up to the time rewards were last added
                 vestedBalance = _vestedBalance(
                     vBZRXBalance,
                     _vBZRXLastUpdate[account],
@@ -644,7 +644,7 @@ contract StakingV1 is StakingState, StakingConstants {
                     )
                 ).div(_startingVBZRXBalance) / 2;
 
-            // user is attributed to a staked balance of vested BZRX, from their last update to the present
+            // user is attributed a staked balance of vested BZRX, from their last update to the present
             totalVotes = _vestedBalance(
                 vBZRXBalance,
                 _vBZRXLastUpdate[account],
@@ -747,7 +747,7 @@ contract StakingV1 is StakingState, StakingConstants {
 
     function sweepFees()
         public
-        checkPause
+        // sweepFeesByAsset() does checkPause
         returns (uint256 bzrxRewards, uint256 crv3Rewards)
     {
         return sweepFeesByAsset(currentFeeTokens);
