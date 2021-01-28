06f22e4504aeccfe2ea54803d8bd0db3e0a63628
Roman Iftodi <romeo8881@gmail.com>
2021-01-28 19:57:56 +0200

    merged

--- testsmainnet/staking/test_StakingNegative.py
+++ testsmainnet/staking/test_StakingNegative.py
@@ -16,38 +16,44 @@ def setFeesController(bzx, stakingV1):
 
 
 @pytest.fixture(scope="module")
-def vBZRX(accounts):
-    vBZRX = loadContractFromEtherscan(
-        "0xb72b31907c1c95f3650b64b2469e08edacee5e8f", "vBZRX")
+def vBZRX(accounts, TestToken):
+    vBZRX = loadContractFromAbi(
+        "0xb72b31907c1c95f3650b64b2469e08edacee5e8f", "vBZRX", TestToken.abi)
     vBZRX.transfer(accounts[0], 1000*10**18, {'from': vBZRX.address})
     return vBZRX
 
 
 @pytest.fixture(scope="module")
-def BZRX(accounts):
-    BZRX = loadContractFromEtherscan(
-        "0x56d811088235F11C8920698a204A5010a788f4b3", "BZRX")
+def BZRX(accounts, TestToken):
+    BZRX = loadContractFromAbi(
+        "0x56d811088235F11C8920698a204A5010a788f4b3", "BZRX", TestToken.abi)
     BZRX.transfer(accounts[0], 1000*10**18, {'from': BZRX.address})
     return BZRX
 
 
 @pytest.fixture(scope="module")
-def iBZRX(accounts, BZRX):
-    iBZRX = loadContractFromEtherscan(
-        "0x18240BD9C07fA6156Ce3F3f61921cC82b2619157", "iBZRX")
+def iBZRX(accounts, BZRX, LoanTokenLogicStandard):
+    iBZRX = loadContractFromAbi(
+        "0x18240BD9C07fA6156Ce3F3f61921cC82b2619157", "iBZRX", LoanTokenLogicStandard.abi)
 
     BZRX.approve(iBZRX, 10*10**50, {'from': accounts[0]})
     iBZRX.mint(accounts[0], 100*10**18, {'from': accounts[0]})
     return iBZRX
 
-
-def loadContractFromEtherscan(address, alias):
+def loadContractFromAbi(address, alias, abi):
     try:
         return Contract(alias)
     except ValueError:
-        contract = Contract.from_explorer(address)
-        contract.set_alias(alias)
+        contract = Contract.from_abi(alias, address=address, abi=abi)
         return contract
+
+# def loadContractFromEtherscan(address, alias):
+#     try:
+#         return Contract(alias)
+#     except ValueError:
+#         contract = Contract.from_explorer(address)
+#         contract.set_alias(alias)
+#         return contract
 
 
 def testStakeCountMismatch(requireMainnetFork, stakingV1, bzx, setFeesController, BZRX, vBZRX, iBZRX):
