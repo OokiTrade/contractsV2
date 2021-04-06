b432e08c904c13366e44d6d7cbbb2120541df581
esvet <shvets.evghenii@gmail.com>
2021-04-06 23:07:32 +0300

    chaged mesterchef to real one

--- scripts/set-masterchef.py
+++ scripts/set-masterchef.py
@@ -6,12 +6,43 @@ from brownie import Contract, network
 from brownie.network.contract import InterfaceContainer
 from brownie.network.state import _add_contract, _remove_contract
 
+masterChef = Contract.from_abi("masterChef", address="0x1FDCA2422668B961E162A8849dc0C2feaDb58915", abi=MasterChef.abi, owner=accounts[0])
 
-# bgovToken = myAccount.deploy(BGovToken)
+
+WBNB = Contract.from_abi("USDT", "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c", interface.IWBNB.abi)
+ETH = Contract.from_abi("USDT", "0x2170ed0880ac9a755fd29b2688956bd959f933f8", TestToken.abi)
+BUSD = Contract.from_abi("BUSD", "0xe9e7cea3dedca5984780bafc599bd69add087d56", TestToken.abi)
+WBTC = Contract.from_abi("USDT", "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c", TestToken.abi)
+USDT = Contract.from_abi("USDT", "0x55d398326f99059ff775485246999027b3197955", TestToken.abi)
+BZRX = Contract.from_abi("BZRX", "0x4b87642AEDF10b642BE4663Db842Ecc5A88bf5ba", TestToken.abi)
+# two sided
+#BZRX_wBNB = Contract.from_abi("BZRX", "0x091A7065306fa5F91a378e8D6858996C20868611", interface.IPancakePair.abi)
+BGOV_wBNB = Contract.from_abi("BGOV_wBNB", "0xEcd0aa12A453AE356Aba41f62483EDc35f2290ed", interface.IPancakePair.abi)
+
+bzx = Contract.from_abi("bzx", address="0xc47812857a74425e2039b57891a3dfcf51602d5d",
+                        abi=interface.IBZx.abi, owner=accounts[0])
+
+iWBNBAddress = bzx.underlyingToLoanPool(WBNB)
+iETHAddress = bzx.underlyingToLoanPool(ETH)
+iBUSDAddress = bzx.underlyingToLoanPool(BUSD)
+iWBTCAddress = bzx.underlyingToLoanPool(WBTC)
+iUSDTAddress = bzx.underlyingToLoanPool(USDT)
+iBZRXAddress = bzx.underlyingToLoanPool(BZRX)
+
+iWBNB = Contract.from_abi("iWBNB", address=iWBNBAddress, abi=LoanTokenLogicWeth.abi, owner=accounts[0])
+iETH = Contract.from_abi("iETH", address=iETHAddress, abi=LoanTokenLogicStandard.abi, owner=accounts[0])
+iBUSD = Contract.from_abi("iBUSD", address=iBUSDAddress, abi=LoanTokenLogicStandard.abi, owner=accounts[0])
+iWBTC = Contract.from_abi("iWBTC", address=iWBTCAddress, abi=LoanTokenLogicStandard.abi, owner=accounts[0])
+iUSDT = Contract.from_abi("iUSDT", address=iUSDTAddress, abi=LoanTokenLogicStandard.abi, owner=accounts[0])
+iBZRX = Contract.from_abi("iBZRX", address=iBZRXAddress, abi=LoanTokenLogicStandard.abi, owner=accounts[0])
+
+bgovToken = Contract.from_abi("BGOV", address="0xf8E026dC4C0860771f691EcFFBbdfe2fa51c77CF", abi=BGovToken.abi, owner=accounts[0])
 
 if(bgovToken.owner() != masterChef.owner()):
     bgovToken.transferOwnership(masterChef, {'from': '0x1FDCA2422668B961E162A8849dc0C2feaDb58915'})
 
+if(masterChef.owner() != accounts[0].address):
+    masterChef.transferOwnership('0x66aB6D9362d4F35596279692F0251Db635165871', {'from':'0xB7F72028D9b502Dc871C444363a7aC5A52546608'})
 
 # # TODO @Tom farm configuration
 # devAccount = myAccount
