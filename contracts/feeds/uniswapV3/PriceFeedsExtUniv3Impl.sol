4f7fc6a4be8be4dc36d5fd03cb44d5017b9846da
Roman Iftodi <romeo8881@gmail.com>
2021-06-17 16:21:32 +0300

    cleanup

--- contracts/feeds/uniswapV3/PriceFeedsExtUniv3Impl.sol
+++ contracts/feeds/uniswapV3/PriceFeedsExtUniv3Impl.sol
@@ -11,52 +11,23 @@ import '@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol';
 
 contract PriceFeedsExtUniv3Impl is IPriceFeedsExt {
   uint32 public period;
+  uint128 public baseAmount;
   address public pool;
   address public tokenA;
   address public tokenB;
 
-  constructor(address _pool, uint32 _period, address _tokenA, address _tokenB) {
-      pool = _pool;
+  constructor(address _pool, uint32 _period, address _tokenA, address _tokenB, uint128 _baseAmount) {
       period = _period;
+      baseAmount = _baseAmount;
+      pool = _pool;
       tokenA = _tokenA;
       tokenB = _tokenB;
+      
   }
-  event Logger(string name, uint256 amount);
-  function latestAnswer() external view override returns (int256){
 
+  function latestAnswer() external view override returns (int256){
     int24 timeWeightedAverageTick = OracleLibrary.consult(pool, period);
-    uint256 quoteAmount = OracleLibrary.getQuoteAtTick(timeWeightedAverageTick, 1e6, tokenB, tokenA);
+    uint256 quoteAmount = OracleLibrary.getQuoteAtTick(timeWeightedAverageTick, baseAmount, tokenB, tokenA);
     return int256(quoteAmount);
   }
-
-  // function latestAnswer2() external view returns (uint256){
-
-  //   int24 timeWeightedAverageTick = OracleLibrary.consult(pool, period);
-  //   uint256 quoteAmount = OracleLibrary.getQuoteAtTick(timeWeightedAverageTick, 1, tokenB, tokenA);
-  //   return quoteAmount;
-  // }
-
-  // function latestAnswer3() external view returns (uint256){
-
-  //   int24 timeWeightedAverageTick = OracleLibrary.consult(pool, period);
-  //   uint256 quoteAmount = OracleLibrary.getQuoteAtTick(timeWeightedAverageTick, 1e6, tokenA, tokenB);
-  //   return quoteAmount;
-  // }
-
-  // function debug(uint32 _period) public view returns(int24){
-  //   return OracleLibrary.consult(pool, _period);
-  // }
-
-  // function debug2(int24 tick) public view returns(uint256){
-  //   return OracleLibrary.getQuoteAtTick(tick, 1e18, tokenA, tokenB);
-  // }
-
-  
-  // function debug3(int24 tick, uint128 precision) public view returns(uint256){
-  //   return OracleLibrary.getQuoteAtTick(tick, precision, tokenB, tokenA);
-  // }
-
-  // function shift(uint256 val) public view returns(int256){
-  //   return int256(val * 1e14);
-  // }
 }
