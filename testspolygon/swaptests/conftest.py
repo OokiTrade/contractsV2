4455205216ff41ec2bd59bf0fe34779499a69f5a
Tom Bean <tomcbean@gmail.com>
2021-07-19 11:41:37 -0400

    moved tests to correct folders

--- testspolygon/swaptests/conftest.py
+++ testspolygon/swaptests/conftest.py
@@ -5,58 +5,58 @@ from brownie import Contract, network, Wei
 from brownie.network.state import _add_contract, _remove_contract
 
 @pytest.fixture(scope="class")
-def requireMainnetFork():
-    assert (network.show_active().find("mainnet-fork")>=0)
+def requireMaticFork():
+    assert (network.show_active().find("matic-fork")>=0)
 
 
 @pytest.fixture(scope="class", autouse=True)
 def ETH(accounts, TestToken):
-    return Contract.from_abi("ETH", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", TestToken.abi)
+    return Contract.from_abi("ETH", "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", TestToken.abi)
 
 @pytest.fixture(scope="class", autouse=True)
 def USDC(accounts, TestToken):
-    return Contract.from_abi("USDC", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", TestToken.abi)
+    return Contract.from_abi("USDC", "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", TestToken.abi)
 
 @pytest.fixture(scope="class", autouse=True)
 def WBTC(accounts, TestToken):
-    return Contract.from_abi("WBTC", "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599", TestToken.abi)
+    return Contract.from_abi("WBTC", "0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6", TestToken.abi)
 
 
 @pytest.fixture(scope="class", autouse=True)
 def USDT(accounts, TestToken):
-    return Contract.from_abi("USDT", "0xdAC17F958D2ee523a2206206994597C13D831ec7", TestToken.abi)
+    return Contract.from_abi("USDT", "0xc2132D05D31c914a87C6611C10748AEb04B58e8F", TestToken.abi)
 
 @pytest.fixture(scope="class", autouse=True)
 def DAI(accounts, TestToken):
-    return Contract.from_abi("DAI", "0x6B175474E89094C44Da98b954EedeAC495271d0F", TestToken.abi)
+    return Contract.from_abi("DAI", "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063", TestToken.abi)
 
 @pytest.fixture(scope="class", autouse=True)
-def LINK(accounts, TestToken):
-    return Contract.from_abi("LINK", "0x514910771AF9Ca656af840dff83E8264EcF986CA", TestToken.abi)
+def WMATIC(accounts, TestToken):
+    return Contract.from_abi("WMATIC", "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", TestToken.abi)
+
 
 @pytest.fixture(scope="class", autouse=True)
-def swaps(accounts, SwapsImplUniswapV2_ETH):
-    swaps = accounts[0].deploy(SwapsImplUniswapV2_ETH)
+def swaps(accounts, SwapsImplUniswapV2_POLYGON):
+    swaps = accounts[0].deploy(SwapsImplUniswapV2_POLYGON)
     return swaps
 
 
 @pytest.fixture(scope="class", autouse=True)
-def bzx(accounts, interface, bZxProtocol, swaps, ProtocolSettings, SwapsExternal,
-        USDC, DAI, WBTC, ETH, USDT, LINK):
-
-    bzx = Contract.from_abi("bzx", address="0xD8Ee69652E4e4838f2531732a46d1f7F584F0b7f", abi=bZxProtocol.abi, owner=accounts[0])
+def bzx(accounts, bZxProtocol, interface, swaps, ProtocolSettings, SwapsExternal,
+        WMATIC,USDC, DAI, WBTC, ETH):
+    bzxproxy = accounts[0].deploy(bZxProtocol)
+    bzx = Contract.from_abi("bzx", address=bzxproxy.address, abi=interface.IBZx.abi, owner=accounts[0])
     _add_contract(bzx)
-    owner = bzx.owner();
-    bzx = Contract.from_abi("bzx", address=bzx.address, abi=interface.IBZx.abi, owner=accounts[0])
-    settings = ProtocolSettings.deploy({'from':owner})
-    bzx.replaceContract(settings.address, {'from':owner})
-    bzx.setSwapsImplContract(swaps, {'from':owner})
+    settings = accounts[0].deploy(ProtocolSettings)
+    bzx.replaceContract(settings.address)
+    bzx.setSwapsImplContract(swaps)
     bzx.setSupportedTokens([
-        USDC.address, DAI.address, WBTC.address, ETH.address, USDT.address, LINK.address
+        WMATIC.address, USDC.address, DAI.address, WBTC.address, ETH.address
     ], [
-        True, True, True, True, True, True
-    ], True, {'from':owner})
-    swapsExternal = SwapsExternal.deploy({'from':owner})
-    bzx.replaceContract(swapsExternal.address, {'from':owner})
+        True, True, True, True, True
+    ], True, {'from':accounts[0]})
+    bzx.replaceContract(accounts[0].deploy(SwapsExternal).address, {'from': accounts[0]})
 
     return bzx
+
+
