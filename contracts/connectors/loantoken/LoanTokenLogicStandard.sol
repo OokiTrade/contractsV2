eef85df6f6f057b980c09add83ad80e0faf5e29b
Roman Hiden <romeo8881@gmail.com>
2021-09-15 13:27:05 +0300

    migrate underlyings; renambe iBZRX -> iOOKI

--- contracts/connectors/loantoken/LoanTokenLogicStandard.sol
+++ contracts/connectors/loantoken/LoanTokenLogicStandard.sol
@@ -1292,25 +1292,3 @@ contract LoanTokenLogicStandard is AdvancedToken {
         }
     }
 }
-
-/*
-pragma solidity 0.5.16;
-
-contract ArbitraryCaller {
-    function sendCall(
-        address target,
-        bytes calldata callData)
-        external
-        payable
-    {
-        (bool success,) = target.call.value(msg.value)(callData);
-        assembly {
-            let size := returndatasize()
-            let ptr := mload(0x40)
-            returndatacopy(ptr, 0, size)
-            if eq(success, 0) { revert(ptr, size) }
-            return(ptr, size)
-        }
-    }
-}
-*/
---
+++ contracts/utils/ArbitraryCaller.sol
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2017-2020, bZeroX, LLC <https://bzx.network/>. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0.
+ */
+
+pragma solidity 0.5.16;
+
+contract ArbitraryCaller {
+    function sendCall(address target, bytes calldata callData)
+        external
+        payable
+    {
+        (bool success, ) = target.call.value(msg.value)(callData);
+        assembly {
+            let size := returndatasize()
+            let ptr := mload(0x40)
+            returndatacopy(ptr, 0, size)
+            if eq(success, 0) {
+                revert(ptr, size)
+            }
+            return(ptr, size)
+        }
+    }
+}
