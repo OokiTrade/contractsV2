a1f109e540996f1977368e31ccd0d22c29115f25
esvet <shvets.evghenii@gmail.com>
2021-09-29 23:19:38 +0300

    pause fee extractor

--- scripts/okki-migration/deploy-migrator-bsc.py
+++ scripts/okki-migration/deploy-migrator-bsc.py
@@ -1,6 +1,9 @@
 exec(open("./scripts/env/set-bsc.py").read())
 
 deployer = CHEF.owner();
-swapRate = 19e6
+
+swapRate = 19e6 #19 gov per bzrx
 govConverter = FixedSwapTokenConverter.deploy(BGOV, BZRX, swapRate, {'from':  deployer})
+FEE_EXTRACTOR = Contract.from_abi("ext", address=BZX.feesController(), abi=FeeExtractAndDistribute_BSC.abi)
+FEE_EXTRACTOR.togglePause(True, {'from': deployer})
 
