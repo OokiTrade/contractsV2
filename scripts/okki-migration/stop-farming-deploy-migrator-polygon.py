b75142569581b5c3713270878696096fd5a0be47
esvet <shvets.evghenii@gmail.com>
2021-09-29 22:27:05 +0300

    upgraded masterchef (disabled vestinng) + deploy converters scripts

---
+++ scripts/okki-migration/stop-farming-deploy-migrator-polygon.py
@@ -0,0 +1,19 @@
+exec(open("./scripts/env/set-matic.py").read())
+
+deployer = CHEF.owner();
+masterChefProxy = Contract.from_abi("masterChefProxy", address=CHEF, abi=Proxy.abi)
+masterChefImpl = MasterChef_Polygon.deploy({'from': deployer})
+masterChefProxy.replaceImplementation(masterChefImpl, {'from': deployer})
+CHEF.setInitialAltRewardsPerShare({'from': deployer})
+CHEF.toggleVestingEnabled(False, {'from': deployer})
+
+CHEF.setLocked(0, False, {'from': deployer})
+CHEF.setLocked(2, False, {'from': deployer})
+CHEF.setGOVPerBlock(0, {'from': deployer})
+
+swapRate = 19e6
+govConverter = FixedSwapTokenConverter.deploy(PGOV, BZRX, swapRate, {'from':  deployer})
+
+
+
+
