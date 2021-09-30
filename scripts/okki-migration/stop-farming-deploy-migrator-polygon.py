7f9ca55c236e0217555f626b82205e239d192d96
esvet <shvets.evghenii@gmail.com>
2021-10-01 00:19:35 +0300

    supports multi token

--- scripts/okki-migration/stop-farming-deploy-migrator-polygon.py
+++ scripts/okki-migration/stop-farming-deploy-migrator-polygon.py
@@ -11,10 +11,13 @@ CHEF.setLocked(0, False, {'from': deployer})
 CHEF.setLocked(2, False, {'from': deployer})
 CHEF.setGOVPerBlock(0, {'from': deployer})
 
-swapRate = 19e6
-govConverter = FixedSwapTokenConverter.deploy(PGOV, BZRX, swapRate, {'from':  deployer})
-
-
 FEE_EXTRACTOR = Contract.from_abi("ext", address=BZX.feesController(), abi=FeeExtractAndDistribute_Polygon.abi)
 FEE_EXTRACTOR.togglePause(True, {'from': deployer})
+
+govOokiConnverter = FixedSwapTokenConverter.deploy(
+    [PGOV, BZRX],
+    [1e6/1.9, 10e6], #19 gov == 1 bzrx == 10 ooki, 1 bzrx = 10 ooki
+    OOKI,
+    {'from':  deployer}
+)
 
