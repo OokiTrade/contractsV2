b160655ebb847058a78fc13866106b33f9b08fd9
Drypto/ICantThinkofAName <arenbalian13@gmail.com>
2021-10-13 10:05:02 -0700

    automate baseAmount selection

--- contracts/feeds/uniswapV3/PriceFeedsExtUniv3Impl.sol
+++ contracts/feeds/uniswapV3/PriceFeedsExtUniv3Impl.sol
@@ -8,16 +8,17 @@ pragma solidity >=0.5.0 <0.8.0;
 
 import "../IPriceFeedsExt.sol";
 import '@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol';
-
+import "@openzeppelin-3.4.0/token/ERC20/ERC20.sol";
 contract PriceFeedsExtUniv3Impl is IPriceFeedsExt {
   uint32 public period;
   uint128 public baseAmount;
   address public pool;
   address public token0;
   address public token1;
-  constructor(address _pool, uint32 _period, address _tokenA, address _tokenB, uint128 _baseAmount) {
+  constructor(address _pool, uint32 _period, address _tokenA, address _tokenB) {
       period = _period;
-      baseAmount = _baseAmount; //set to 10**token0Decimals;
+	  address token0Address = _tokenA < _tokenB ? _tokenA : _tokenB;
+      baseAmount = uint128(10**(ERC20(token0Address).decimals())); //set to 10**token0Decimals
       pool = _pool;
 	  (token0, token1) = (_tokenA, _tokenB);
       
