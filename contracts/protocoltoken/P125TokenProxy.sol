8f4b8b2ffda138864d325c03c50f04baa358b4bd
Eugene <81377977+eugenesh1@users.noreply.github.com>
2021-12-01 20:13:53 +0200

    added P125 (#173)
    Co-authored-by: esvet <shvets.evghenii@gmail.com>

---
+++ contracts/protocoltoken/P125TokenProxy.sol
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2017-2021, OokiDao. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0.
+ */
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../proxies/0_8/Upgradeable_0_8.sol";
+import "@openzeppelin-4.3.2/utils/Address.sol";
+
+contract P125TokenProxy is Upgradeable_0_8 {
+    constructor(address _impl) public {
+        replaceImplementation(_impl);
+    }
+
+    fallback() external {
+        address impl = implementation;
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), impl, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+
+    function replaceImplementation(address impl) public onlyOwner {
+        require(Address.isContract(impl), "not a contract");
+        implementation = impl;
+    }
+}
