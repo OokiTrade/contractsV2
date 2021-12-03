8c60ffc189ef86597865dccae5d8bab04d88e592
Roman Iftodi <romeo8881@gmail.com>
2021-12-03 16:58:51 +0200

    import

--- contracts/utils/AdminLock.sol
+++ contracts/utils/AdminLock.sol
@@ -9,9 +9,9 @@ import "@openzeppelin-4.3.2/access/Ownable.sol";
 import "@openzeppelin-4.3.2/token/ERC20/utils/SafeERC20.sol";
 
 contract AdminLock is Ownable {
+    using SafeERC20 for IERC20;
  
     function rescue(IERC20 _token) public onlyOwner {
         _token.safeTransfer(msg.sender, _token.balanceOf(address(this)));
     }
-
 }
