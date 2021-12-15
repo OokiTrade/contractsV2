34aece5271de9fe0af1f163e6a869e4081c06825
RomanH <romeo8881@gmail.com>
2021-12-15 21:13:53 +0200

    ooip-6 treasury convert

---
+++ scripts/env/common-functions.py
@@ -0,0 +1,15 @@
+# global functions
+from ape_safe import ApeSafe
+def createGnosisTx(safe:ApeSafe, target, calldata, index=0):
+  to = target
+  value = 0
+  data = calldata
+  operation = 0
+  safe_nonce = safe.pending_nonce() # TODO handle custom index in case of replace
+  safeTx = safe.build_multisig_tx(to, value, data, operation, safe_nonce=safe_nonce)
+  print(safeTx)
+  signature = safe.sign_with_frame(safeTx)
+  safe.post_transaction(safeTx)
+  # index = index + 1
+  # safe.preview(safeTx)
+  # assert False
