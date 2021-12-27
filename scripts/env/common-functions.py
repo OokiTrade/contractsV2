63158d94ec9b6cc363305f5d535201f13549c017
Roman Hiden <romeo8881@gmail.com>
2021-12-27 15:47:34 +0200

    updated env

--- scripts/env/common-functions.py
+++ scripts/env/common-functions.py
@@ -5,11 +5,12 @@ def createGnosisTx(safe:ApeSafe, target, calldata, index=0):
   value = 0
   data = calldata
   operation = 0
-  safe_nonce = safe.pending_nonce() # TODO handle custom index in case of replace
+  safe_nonce = index # TODO handle custom index in case of replace
   safeTx = safe.build_multisig_tx(to, value, data, operation, safe_nonce=safe_nonce)
   print(safeTx)
-  signature = safe.sign_with_frame(safeTx)
-  safe.post_transaction(safeTx)
+  # signature = safe.sign_with_frame(safeTx)
+  # safe.post_transaction(safeTx)
+  return safeTx
   # index = index + 1
   # safe.preview(safeTx)
   # assert False
