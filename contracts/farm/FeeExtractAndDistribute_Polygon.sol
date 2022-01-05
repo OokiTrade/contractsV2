31000e0ba2b697e711e55b2f7f28ee40023d7892
Drypto/ICantThinkofAName <arenbalian13@gmail.com>
2022-01-04 19:08:37 -0800

    formatting

--- contracts/farm/FeeExtractAndDistribute_Polygon.sol
+++ contracts/farm/FeeExtractAndDistribute_Polygon.sol
@@ -15,7 +15,7 @@ contract FeeExtractAndDistribute_Polygon is Upgradeable_0_8 {
     IBZx public constant bZx = IBZx(0x059D60a9CEfBc70b9Ea9FFBb9a041581B1dFA6a8);
 
     address public constant MATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
-	address public constant USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+    address public constant USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
     IUniswapV2Router public constant swapsRouterV2 =
         IUniswapV2Router(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506); // Sushiswap
 
@@ -80,28 +80,30 @@ contract FeeExtractAndDistribute_Polygon is Upgradeable_0_8 {
         uint256 amount;
         for (uint256 i = 0; i < assets.length; i++) {
             asset = assets[i];
-			if(asset==USDC)continue; //USDC already accounted for
+            if (asset == USDC) continue; //USDC already accounted for
             amount = exportedFees[asset];
             exportedFees[asset] = 0;
 
             if (amount != 0) {
-				usdcOutput += asset==MATIC?_swapWithPair([asset,USDC], amount):_swapWithPair([asset,MATIC,USDC], amount); //builds route for all tokens to route through MATIC
+                usdcOutput += asset == MATIC
+                    ? _swapWithPair([asset, USDC], amount)
+                    : _swapWithPair([asset, MATIC, USDC], amount); //builds route for all tokens to route through MATIC
             }
         }
 
         if (usdcOutput != 0) {
-			IERC20(USDC).transfer(treasuryWallet, usdcOutput); //transfer to treasury/multisig
-			emit ExtractAndDistribute(usdcOutput, 0); //for tracking distribution amounts
+            IERC20(USDC).transfer(treasuryWallet, usdcOutput); //transfer to treasury/multisig
+            emit ExtractAndDistribute(usdcOutput, 0); //for tracking distribution amounts
         }
     }
 
-    function _swapWithPair(
-        address[2] memory route,
-        uint256 inAmount
-    ) internal returns (uint256 returnAmount) {
-		address[] memory path = new address[] (2);
-		path[0] = route[0];
-		path[1] = route[1];
+    function _swapWithPair(address[2] memory route, uint256 inAmount)
+        internal
+        returns (uint256 returnAmount)
+    {
+        address[] memory path = new address[](2);
+        path[0] = route[0];
+        path[1] = route[1];
         uint256[] memory amounts = swapsRouterV2.swapExactTokensForTokens(
             inAmount,
             1, // amountOutMin
@@ -112,14 +114,15 @@ contract FeeExtractAndDistribute_Polygon is Upgradeable_0_8 {
 
         returnAmount = amounts[1];
     }
-    function _swapWithPair(
-        address[3] memory route,
-        uint256 inAmount
-    ) internal returns (uint256 returnAmount) {
-		address[] memory path = new address[] (3);
-		path[0] = route[0];
-		path[1] = route[1];
-		path[2] = route[2];
+
+    function _swapWithPair(address[3] memory route, uint256 inAmount)
+        internal
+        returns (uint256 returnAmount)
+    {
+        address[] memory path = new address[](3);
+        path[0] = route[0];
+        path[1] = route[1];
+        path[2] = route[2];
         uint256[] memory amounts = swapsRouterV2.swapExactTokensForTokens(
             inAmount,
             1, // amountOutMin
