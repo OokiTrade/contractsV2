076526840b8b487996cec3d829adef8808ac9299
Drypto/ICantThinkofAName <arenbalian13@gmail.com>
2022-01-08 12:57:06 -0800

    Formatting and Price Fix

--- contracts/farm/BuyBackAndBurn.sol
+++ contracts/farm/BuyBackAndBurn.sol
@@ -21,9 +21,9 @@ contract BuyBackAndBurn is Upgradeable_0_8 {
 
     address public constant USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
     address public constant P125 = 0x83000597e8420aD7e9EDD410b2883Df1b83823cF;
-    IUniswapV3SwapRouter public constant swapsRouterV3 =
+    IUniswapV3SwapRouter public constant SWAP_ROUTER =
         IUniswapV3SwapRouter(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45); //uni v3
-    IQuoter public constant quoteContract =
+    IQuoter public constant QUOTER =
         IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6); //uni v3 quote v2
     uint256 public constant WEI_PRECISION_PERCENT = 10**20; //1e18 precision on percentages
 
@@ -44,7 +44,7 @@ contract BuyBackAndBurn is Upgradeable_0_8 {
     }
 
     function getDebtTokenAmountOut(uint256 amountIn) public returns (uint256) {
-        return quoteContract.quoteExactInput(specsForTWAP.route, amountIn);
+        return QUOTER.quoteExactInput(specsForTWAP.route, amountIn);
     }
 
     function worstExecPrice() public view returns (uint256) {
@@ -57,23 +57,30 @@ contract BuyBackAndBurn is Upgradeable_0_8 {
         uint256 fullBalance = IERC20Metadata(USDC).balanceOf(address(this));
         uint256 minAmountOut = (worstExecPrice() * fullBalance) / 10**6;
         uint256 balanceUsed;
+        uint256 execPrice;
         if (getDebtTokenAmountOut(fullBalance) >= minAmountOut) {
-            (balanceUsed, buyAmount) = _buyDebtToken(WEI_PRECISION_PERCENT); //uses full amount
+            (execPrice, balanceUsed, buyAmount) = _buyDebtToken(
+                WEI_PRECISION_PERCENT
+            ); //uses full amount
         } else {
-            (balanceUsed, buyAmount) = _buyDebtToken(percentage); //uses partial
+            (execPrice, balanceUsed, buyAmount) = _buyDebtToken(percentage); //uses partial
         }
-        emit BuyBack(
-            getDebtTokenAmountOut(balanceUsed),
-            balanceUsed,
-            buyAmount
-        );
+        emit BuyBack(execPrice, balanceUsed, buyAmount);
     }
 
-    function _buyDebtToken(uint256 percentage) internal returns (uint256, uint256) {
+    function _buyDebtToken(uint256 percentage)
+        internal
+        returns (
+            uint256,
+            uint256,
+            uint256
+        )
+    {
         uint256 bought;
         uint256 balanceUsed = (IERC20Metadata(USDC).balanceOf(address(this)) *
             percentage) / WEI_PRECISION_PERCENT;
         uint256 minAmountOut = (worstExecPrice() * balanceUsed) / 10**6;
+        uint256 execPrice = getDebtTokenAmountOut(balanceUsed);
         require(
             minAmountOut / 10**12 >= balanceUsed,
             "worst absolue price is 1:1"
@@ -85,7 +92,7 @@ contract BuyBackAndBurn is Upgradeable_0_8 {
                 amountIn: balanceUsed,
                 amountOutMinimum: minAmountOut
             });
-        return (balanceUsed, swapsRouterV3.exactInput(params));
+        return (execPrice, balanceUsed, SWAP_ROUTER.exactInput(params));
     }
 
     // OnlyOwner functions
@@ -102,8 +109,8 @@ contract BuyBackAndBurn is Upgradeable_0_8 {
     }
 
     function setApproval() public onlyOwner {
-        IERC20Metadata(USDC).approve(address(swapsRouterV3), 0);
-        IERC20Metadata(USDC).approve(address(swapsRouterV3), type(uint256).max);
+        IERC20Metadata(USDC).approve(address(SWAP_ROUTER), 0);
+        IERC20Metadata(USDC).approve(address(SWAP_ROUTER), type(uint256).max);
     }
 
     function setPriceGetter(IPriceGetterP125 _wallet) public onlyOwner {
