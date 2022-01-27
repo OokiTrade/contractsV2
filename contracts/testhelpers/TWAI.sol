7e74c928b516ad5718acfef0cdc6ee48efdefd09
RomanH <romeo8881@gmail.com>
2022-01-27 11:31:01 +0200

    updates

--- contracts/testhelpers/TWAI.sol
+++ contracts/testhelpers/TWAI.sol
@@ -21,18 +21,26 @@ contract TWAI {
         lastIR = _lastIR;
     }
 
-    function getInterestRate(uint256 utilizationRate, uint256 interestRate) public pure returns(uint256 newInterestRate){
+    function getInterestRate(uint256 interestRate) public pure returns(uint256 newInterestRate){
         (uint256 a, uint256 b) = getAB(interestRate);
         
-        uint256 highestInterestRateAt100Percent = 1.2e18;
-        uint256 targetUtilizationRate = 0.8e18;
+        uint256 e = 2.7e18;
 
-        return (1000 * (((interestRate/highestInterestRateAt100Percent) ** uint256(1/1000)) - 1))/(targetUtilizationRate-1e18);
-        //  (1000 * (((0.2/1.2) ** (1/1000)) - 1))/(0.8-0.9)
+        return (a*e/1e18)**(b*interestRate/1e36);
 
     }
 
     function getAB(uint256 interestRate) public pure returns (uint256 a, uint256 b) {
+        // here
+        uint256 utilRate1 = 0.8e18;
+        uint256 utilRate2 = 1e18;
+        uint256 intRate1 = 0.2e18;
+        uint256 intRate2 = 1.2e18;
+        // y = (1000 * (((0.2/1.2) ** (1/1000)) - 1))/(0.8-0.9)
+        // x = 0.2/e**(0.8 * y)
+
+        uint256 a;
+        a = (1 - (1000 * (((intRate1*1e18/intRate2) ** (1/1000)) )/(utilRate2 - utilRate1)
         return  (1e18, 1e18);
     }
 }
