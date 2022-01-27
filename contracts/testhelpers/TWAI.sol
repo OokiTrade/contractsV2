e4485bda8017dad43dd04e84b500d5528cf86441
RomanH <romeo8881@gmail.com>
2022-01-27 20:35:25 +0200

    small comment

--- contracts/testhelpers/TWAI.sol
+++ contracts/testhelpers/TWAI.sol
@@ -59,7 +59,7 @@ contract TWAI {
         uint256 utilRate1 = 0.8e18;
         uint256 utilRate2 = 1e18;
         uint256 intRate1 = interestRate;
-        uint256 intRate2 = 1.2e18;
+        uint256 intRate2 = 1.2e18; // TODO this actually states that at 100% utilization we have 120 interest rate. I believe this should be 200%
         // y = (1000 * (((0.2/1.2) ** (1/1000)) - 1))/(0.8-0.9)
         // x = 0.2/e**(0.8 * y)
 
