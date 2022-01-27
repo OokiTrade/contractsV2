44ee87cf4f0f39ba6b7ba2c6731cf801653c6776
RomanH <romeo8881@gmail.com>
2022-01-27 13:11:59 +0200

    updates

--- contracts/testhelpers/TWAI.sol
+++ contracts/testhelpers/TWAI.sol
@@ -34,13 +34,13 @@ contract TWAI {
         // here
         uint256 utilRate1 = 0.8e18;
         uint256 utilRate2 = 1e18;
-        uint256 intRate1 = 0.2e18;
+        uint256 intRate1 = interestRate;
         uint256 intRate2 = 1.2e18;
         // y = (1000 * (((0.2/1.2) ** (1/1000)) - 1))/(0.8-0.9)
         // x = 0.2/e**(0.8 * y)
 
         uint256 a;
-        a = (1 - (1000 * (((intRate1*1e18/intRate2) ** (1/1000)) )/(utilRate2 - utilRate1)
+        // a = (1 - (1000 * (((intRate1*1e18/intRate2) ** (1/1000)) )/(utilRate2 - utilRate1);
         return  (1e18, 1e18);
     }
 }
