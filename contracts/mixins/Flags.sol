a0f98af3bdce6bb275b7d100b3162232b63ae17a
Drypto | OOKI <51417606+Drypto13@users.noreply.github.com>
2022-03-14 13:11:23 -0700

    Delegated Manager (#217)
    * set delegated manager in loan opening
    
    * test case and improvements

--- contracts/mixins/Flags.sol
+++ contracts/mixins/Flags.sol
@@ -1,5 +1,6 @@
-pragma solidity 0.5.17;
+pragma solidity >=0.5.17 <0.9.0;
 
 contract Flags {
     uint128 public constant DEX_SELECTOR_FLAG = 2; // base-2: 10
+    uint128 public constant DELEGATE_FLAG = 4;
 }
