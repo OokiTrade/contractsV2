d9a40fe4dc2b8734cfb71afdf825c025901bd1e9
RomanH <romeo8881@gmail.com>
2022-04-02 18:00:28 +0300

    deploying arbitrum; modified Constants.sol

--- scripts/deployment/interest-twap-arbitrum.py
+++ scripts/deployment/interest-twap-arbitrum.py
@@ -15,15 +15,15 @@ cui = CurvedInterestRate.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")}
 # <TickMath Contract '0x37A3fC76105c51D54a9c1c3167e30601EdeE8782'>
 tickMath = TickMath.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
 
-# <LoanMaintenance_Arbitrum Contract '0x2F3A1964E1e5959B4f006bE062479B24fC806BB0'>
+# INVALID <LoanMaintenance_Arbitrum Contract '0x2F3A1964E1e5959B4f006bE062479B24fC806BB0'>
 loanMaintenance_Arbitrum = LoanMaintenance_Arbitrum.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
-# <LoanMaintenance Contract '0xAaD3b6e314b3b31BE6d42A5D8eFfAe4cDB6d2d4F'>
+# INVALID <LoanMaintenance Contract '0xAaD3b6e314b3b31BE6d42A5D8eFfAe4cDB6d2d4F'>
 loanMaintenance = LoanMaintenance.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
-# <LoanMaintenance_2 Contract '0x7fC67DAA325BEC82e829685290aeec990f412AB2'>
+# INVALID <LoanMaintenance_2 Contract '0x7fC67DAA325BEC82e829685290aeec990f412AB2'>
 loanMaintenance_2 = LoanMaintenance_2.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
-# <LoanOpenings Contract '0xD36913f0225E64C4689b5D6144CeF952d1ad23dA'>
+# INVALID <LoanOpenings Contract '0xD36913f0225E64C4689b5D6144CeF952d1ad23dA'>
 loanOpenings = LoanOpenings.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
-# <LoanSettings Contract '0x49743dA77Ff019424E2e153A0712eD87fFDB74Eb'>
+# INVALID <LoanSettings Contract '0x49743dA77Ff019424E2e153A0712eD87fFDB74Eb'>
 loanSettings = LoanSettings.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
 
 
