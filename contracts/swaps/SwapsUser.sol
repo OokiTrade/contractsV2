ffa1f5e03fce46ce8300d36e919b3f5afec662b7
Drypto13 <arenbalian13@gmail.com>
2022-04-13 11:03:27 -0700

    hold for OOKI fee fix

--- contracts/swaps/SwapsUser.sol
+++ contracts/swaps/SwapsUser.sol
@@ -136,6 +136,9 @@ contract SwapsUser is State, SwapsEvents, FeesHelper, Flags {
                 if (loanDataBytes.length != 0 && abi.decode(loanDataBytes, (uint128)) & PAY_WITH_OOKI_FLAG != 0) {
                     tradingFee = _getTradingFeeWithOOKI(addrs[1], vals[2]);
                     if(tradingFee != 0){
+                        if(abi.decode(loanDataBytes, (uint128)) & HOLD_OOKI_FLAG != 0){
+                            tradingFee = _adjustForHeldBalance(tradingFee, addrs[4]);
+                        }
                         IERC20(OOKI).safeTransferFrom(addrs[4], address(this), tradingFee);
                         _payTradingFee(
                             addrs[4], // user
@@ -149,6 +152,9 @@ contract SwapsUser is State, SwapsEvents, FeesHelper, Flags {
                     tradingFee = _getTradingFee(vals[2]);
 
                     if (tradingFee != 0) {
+                        if(loanDataBytes.length != 0 && abi.decode(loanDataBytes, (uint128)) & HOLD_OOKI_FLAG != 0){
+                            tradingFee = _adjustForHeldBalance(tradingFee, addrs[4]);
+                        }
                         vals[2] = vals[2].add(tradingFee);
                     }
                 }
