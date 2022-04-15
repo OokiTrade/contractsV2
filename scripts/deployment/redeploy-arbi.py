ba226f5acc19202e8f60c996b2db14aee42676b2
RomanH <romeo8881@gmail.com>
2022-04-15 12:41:44 +0300

    lowered gas price

--- scripts/deployment/redeploy-arbi.py
+++ scripts/deployment/redeploy-arbi.py
@@ -3,13 +3,13 @@ deployer = accounts[2]
 tickMath = TickMathV1.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
 
 
-loanMaintenance_Arbitrum = LoanMaintenance.deploy({"from": deployer, "gas_price": Wei("0.6 gwei")})
-loanMaintenance_2 = LoanMaintenance_2.deploy({"from": deployer, "gas_price": Wei("0.6 gwei")})
-loanOpenings = LoanOpenings.deploy({"from": deployer, "gas_price": Wei("0.6 gwei")})
-loanClosings_Arbitrum = LoanClosings.deploy({"from": deployer, "gas_price": Wei("0.6 gwei")})
-loanSettings = LoanSettings.deploy({"from": deployer, "gas_price": Wei("0.6 gwei")})
-swapsImpl = SwapsExternal.deploy({"from": deployer, "gas_price": Wei("0.6 gwei")})
-receiver = Receiver.deploy({"from": deployer, "gas_price": Wei("0.6 gwei")})
+loanMaintenance_Arbitrum = LoanMaintenance.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
+loanMaintenance_2 = LoanMaintenance_2.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
+loanOpenings = LoanOpenings.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
+loanClosings_Arbitrum = LoanClosings.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
+loanSettings = LoanSettings.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
+swapsImpl = SwapsExternal.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
+receiver = Receiver.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
 
 
 BZX.replaceContract(loanMaintenance_Arbitrum, {"from": GUARDIAN_MULTISIG})
@@ -21,8 +21,8 @@ BZX.replaceContract(swapsImpl, {"from": GUARDIAN_MULTISIG})
 BZX.replaceContract(receiver, {"from": GUARDIAN_MULTISIG})
 
 # remember deploy WETH
-loanTokenLogicStandard = LoanTokenLogicStandard.deploy({"from": deployer, "gas_price": Wei("0.6 gwei")})
-loanTokenLogicWeth = LoanTokenLogicWeth.deploy({"from": deployer, "gas_price": Wei("0.6 gwei")})
+loanTokenLogicStandard = LoanTokenLogicStandard.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
+loanTokenLogicWeth = LoanTokenLogicWeth.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
 
 for l in list:
     iTokenTemp = Contract.from_abi("iTokenTemp", l[0], LoanToken.abi)
@@ -33,7 +33,7 @@ for l in list:
         iTokenTemp.setTarget(loanTokenLogicStandard, {"from": GUARDIAN_MULTISIG})
 
 
-ookiPriceFeed = OOKIPriceFeed.deploy({"from": deployer, "gas_price": Wei("0.6 gwei")})
+ookiPriceFeed = OOKIPriceFeed.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
 pricefeeds = Contract.from_abi('priceFeeds',BZX.priceFeeds(),PriceFeeds.abi)
 pricefeeds.setPriceFeed([OOKI],[ookiPriceFeed], {"from": GUARDIAN_MULTISIG})
 
@@ -42,7 +42,7 @@ for l in list:
     calldata = BZX.setupLoanPoolTWAI(l[0], {"from": GUARDIAN_MULTISIG})
 
 
-helperImpl = HelperImpl.deploy({"from": deployer, "gas_price": Wei("0.6 gwei")})
+helperImpl = HelperImpl.deploy({"from": deployer, "gas_price": Wei("0.5 gwei")})
 
 HELPER = Contract.from_abi("HELPER", "0xB8329B5458B1E493EFd8D9DA8C3B5E6D68e67C21", HelperProxy.abi)
 HELPER.replaceImplementation(helperImpl, {"from": GUARDIAN_MULTISIG})
