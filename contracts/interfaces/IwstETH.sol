6cb812ccbec8e4762067caf2b402beea9cec0026
Drypto <arenbalian13@gmail.com>
2022-04-21 09:24:52 -0700

    version changes

--- contracts/interfaces/IwstETH.sol
+++ contracts/interfaces/IwstETH.sol
@@ -1,9 +1,8 @@
-pragma solidity 0.5.17;
+pragma solidity >=0.5.17 <0.9.0;
 
 interface IwstETH {
     function wrap(uint256 _stETHAmount) external returns(uint256);
     function unwrap(uint256 _wstETHAmount) external returns(uint256);
     function getStETHByWstETH(uint256 _wstETHAmount) external view returns(uint256);
     function getWstETHBystETH(uint256 _stETHAmount) external view returns(uint256);
-
 }
