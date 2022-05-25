ed275c4b27dde2ddbf5ab729e4b416fcbc93c408
Drypto13 <arenbalian13@gmail.com>
2022-05-25 09:47:21 -0700

    wbtc swap handling

--- contracts/farm/FeeExtractAndDistribute_Polygon.sol
+++ contracts/farm/FeeExtractAndDistribute_Polygon.sol
@@ -21,9 +21,11 @@ contract FeeExtractAndDistribute_Polygon is PausableGuardian_0_8 {
         0x12EBd8263A54751Aaf9d8C2c74740A8e62C0AfBe;
     address public constant MATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
     address public constant USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+    address public constant WETH = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
+    address public constant WBTC = 0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6;
     uint256 public constant WEI_PRECISION_PERCENT = 10**20;
     uint64 public constant DEST_CHAINID = 1; //to be set
-    uint256 public constant MIN_USDC_AMOUNT = 20e6; //1000 USDC minimum bridge amount
+    uint256 public constant MIN_USDC_AMOUNT = 1000e6; //1000 USDC minimum bridge amount
     IUniswapV2Router public constant SWAPS_ROUTER_V2 =
         IUniswapV2Router(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506); // Sushiswap
 
@@ -82,9 +84,13 @@ contract FeeExtractAndDistribute_Polygon is PausableGuardian_0_8 {
             exportedFees[asset] = 0;
 
             if (amount != 0) {
-                usdcOutput += asset == MATIC
-                    ? _swapWithPair([asset, USDC], amount)
-                    : _swapWithPair([asset, MATIC, USDC], amount); //builds route for all tokens to route through MATIC
+                if (asset == MATIC) {
+                    usdcOutput = _swapWithPair([asset, USDC], amount);
+                } else if (asset == WBTC) {
+                    usdcOutput = _swapWithPair([asset, WETH, USDC], amount);
+                } else {
+                    usdcOutput = _swapWithPair([asset, MATIC, USDC], amount); //builds route for all tokens to route through MATIC
+                }
             }
         }
         if (usdcOutput != 0) {
