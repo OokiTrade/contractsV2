cab9748f6147f3602be648d48017bc829628165b
RomanH <romeo8881@gmail.com>
2022-06-23 13:53:35 +0300

    more tests

--- testsmainnet/itoken-collateral/test_itoken_collateral.py
+++ testsmainnet/itoken-collateral/test_itoken_collateral.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 #!/usr/bin/python3
 
+from imp import load_module
 import pytest
 from brownie import ZERO_ADDRESS, network, Contract, reverts, chain
 from brownie.convert.datatypes import Wei
@@ -26,13 +27,23 @@ def isolation(fn_isolation):
 
 
 @pytest.fixture(scope="module")
-def BZX(accounts, interface, TickMathV1, LoanOpenings, LoanSettings):
+def BZX(accounts, interface, TickMathV1, LoanOpenings, LoanSettings, ProtocolSettings, LoanClosingsLiquidation, LoanMaintenance, LiquidationHelper):
     tickMathV1 = accounts[0].deploy(TickMathV1)
+    liquidationHelper = accounts[0].deploy(LiquidationHelper)
+
     lo = accounts[0].deploy(LoanOpenings)
     ls = accounts[0].deploy(LoanSettings)
+    ps = accounts[0].deploy(ProtocolSettings)
+    lcs = accounts[0].deploy(LoanClosingsLiquidation)
+    lm = accounts[0].deploy(LoanMaintenance)
+
     bzx = Contract.from_abi("bzx", address="0xD8Ee69652E4e4838f2531732a46d1f7F584F0b7f", abi=interface.IBZx.abi)
     bzx.replaceContract(lo, {"from": bzx.owner()})
     bzx.replaceContract(ls, {"from": bzx.owner()})
+    bzx.replaceContract(ps, {"from": bzx.owner()})
+    bzx.replaceContract(lcs, {"from": bzx.owner()})
+    bzx.replaceContract(lm, {"from": bzx.owner()})
+
     return bzx
 
 
@@ -44,10 +55,24 @@ def USDT(accounts, TestToken):
 def USDC(accounts, TestToken):
     return Contract.from_abi("USDC", address="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", abi=TestToken.abi)
 
+@pytest.fixture(scope="module")
+def FRAX(accounts, TestToken):
+    return Contract.from_abi("FRAX", address="0x853d955aCEf822Db058eb8505911ED77F175b99e", abi=TestToken.abi)
+
+@pytest.fixture(scope="module")
+def AAVE(accounts, TestToken):
+    return Contract.from_abi("AAVE", address="0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", abi=TestToken.abi)
+
 
 @pytest.fixture(scope="module")
 def WETH(accounts, TestToken):
     return Contract.from_abi("USDT", address="0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", abi=TestToken.abi)
+
+@pytest.fixture(scope="module")
+def PRICE_FEED(accounts, BZX, PriceFeeds):
+
+    return Contract.from_abi("PRICE_FEED", BZX.priceFeeds(), abi = PriceFeeds.abi)
+    # return Contract.from_abi("USDT", address="0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", abi=TestToken.abi)
 
 
 @pytest.fixture(scope="module")
@@ -82,7 +107,78 @@ def test_cases():
     # Test Case 6: make sure you can't intentionally borrowOrTradeFromPool and create undexpected loanParam
     # Test Case 7: make sure guardian can create/updates existing loan params with specific settings
     # Test Case 8: test HELPER getBorrowAmount for deposit and vice versa
-    assert True 
+    # Test Case 9: mint/burn
+    # Test Case 10: borrow
+    # Test Case 11: marginTrade
+    # Test Case 12: marginTrade with iToken as collateral
+    assert True
+
+def test_case1(accounts, BZX, USDC, USDT, iUSDT, iUSDC, REGISTRY, GUARDIAN_MULTISIG, FRAX, LoanTokenLogicStandard, LoanToken, CurvedInterestRate, PriceFeeds, PRICE_FEED, interface):
+    underlyingSymbol = FRAX.symbol()
+    iTokenSymbol = "i{}".format(underlyingSymbol.upper())
+    iTokenName = "Ooki {} iToken ({})".format(underlyingSymbol, iTokenSymbol)
+    
+    loanTokenLogicStandard = LoanTokenLogicStandard.deploy({'from': GUARDIAN_MULTISIG})
+    iTokenProxy = LoanToken.deploy(GUARDIAN_MULTISIG, loanTokenLogicStandard, {"from": GUARDIAN_MULTISIG})
+    iToken = Contract.from_abi("iToken", address=iTokenProxy,abi=LoanTokenLogicStandard.abi, owner=GUARDIAN_MULTISIG)
+    iToken.initialize(FRAX, iTokenName, iTokenSymbol, {"from": GUARDIAN_MULTISIG})
+    iToken.initializeDomainSeparator({"from": GUARDIAN_MULTISIG})
+    cui = CurvedInterestRate.deploy({'from': GUARDIAN_MULTISIG})
+    iToken.setDemandCurve(cui, {'from': GUARDIAN_MULTISIG})
+
+    
+    # TODO migrate pricefeed to allow guardian to modify, deploy new pricefeed
+    PRICE_FEED.setPriceFeed([FRAX], ['0x14d04Fff8D21bd62987a5cE9ce543d2F1edF5D3E'], {'from': PRICE_FEED.owner()})
+    BZX.setLoanPool([iToken], [iToken.loanTokenAddress()], {'from': GUARDIAN_MULTISIG})
+    BZX.setSupportedTokens([iToken.loanTokenAddress()], [True], True, {'from': GUARDIAN_MULTISIG})
+    BZX.setupLoanPoolTWAI(iToken, {'from': GUARDIAN_MULTISIG})
+
+    # get some frax
+    FRAX.transfer(accounts[0], 100000e18, {"from": "0xd4937682df3c8aef4fe912a96a74121c0829e664"})
+
+    # lend frax
+    iFRAX = Contract.from_abi("iFRAX", address=iToken, abi=interface.IToken.abi)
+    FRAX.approve(iFRAX, 2**256-1, {"from": accounts[0]})
+    iFRAX.mint(accounts[0], 10000e18, {"from": accounts[0]})
+
+    # borrow frax
+    iFRAX.borrow("", 50e18, 0, 1e18, '0x0000000000000000000000000000000000000000', accounts[0], accounts[0], b"", {'from': accounts[0], 'value':1e18})
+    
+    # borrow using frax collateral
+    FRAX.approve(iUSDT, 2**256-1, {"from": accounts[0]})
+    iUSDT.borrow("", 50e6, 0, 100e18, FRAX, accounts[0], accounts[0], b"", {'from': accounts[0]})
+
+    # margin trade frax collateral
+    iUSDT.marginTrade(0, 2e18, 0, 100e18, FRAX, accounts[0], b'',{'from': accounts[0]})
+    
+    # margin trade frax principal
+    USDT.approve(iFRAX, 2**256-1, {"from": accounts[0]})
+    iFRAX.marginTrade(0, 2e18, 0, 50e6, USDT, accounts[0], b'',{'from': accounts[0]})
+
+    assert True
+
+def test_case2(accounts, BZX, USDC, USDT, iUSDT, iUSDC, REGISTRY, GUARDIAN_MULTISIG, FRAX, LoanTokenLogicStandard, LoanToken, CurvedInterestRate, PriceFeeds, PRICE_FEED, interface, AAVE):
+    PRICE_FEED.setPriceFeed([AAVE], ['0x6Df09E975c830ECae5bd4eD9d90f3A95a4f88012'], {'from': PRICE_FEED.owner()})
+    BZX.setSupportedTokens([AAVE], [True], True, {'from': GUARDIAN_MULTISIG})
+
+    # get some AAVE
+    AAVE.transfer(accounts[0], 100000e18, {"from": "0x26a78d5b6d7a7aceedd1e6ee3229b372a624d8b7"})
+
+    # borrow using AAVE as collateral
+    AAVE.approve(iUSDT, 2**256-1, {"from": accounts[0]})
+    iUSDT.borrow("", 50e6, 0, 100e18, AAVE, accounts[0], accounts[0], b"", {'from': accounts[0]})
+    loans = BZX.getUserLoans(accounts[0], 0, 10, 0, 0, 0)
+    BZX.closeWithSwap(loans[0][0], accounts[0], 100e18, True, b"", {"from": accounts[0]})
+
+    # margint trade AAVE collateral
+    iUSDT.marginTrade(0, 2e18, 0, 100e18, AAVE, accounts[0], b'',{'from': accounts[0]})
+    AAVE.approve(BZX, 2**256-1, {"from": accounts[0]})
+    loans = BZX.getUserLoans(accounts[0], 0, 10, 0, 0, 0)
+    BZX.closeWithSwap(loans[0][0], accounts[0], 100e18, True, b"", {"from": accounts[0]})
+
+    # margint trade AAVE principal - you can't since no where to borrow
+
+    assert False
 
 # this checks that migrateLoanParamsList works for all
 def test_case4_1(BZX, USDC, USDT, iUSDT, iUSDC, REGISTRY, GUARDIAN_MULTISIG):
@@ -192,4 +288,4 @@ def test_case5(BZX, USDC, USDT, iUSDT, iUSDC):
     assert loanParams[0][6] == 15000000000000000000
     assert loanParams[0][7] == 0
 
-    assert False
+    assert True
