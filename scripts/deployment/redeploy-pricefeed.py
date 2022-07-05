866078fad050342f85a9c6066803d30f396cd827
RomanH <romeo8881@gmail.com>
2022-07-05 14:14:02 +0300

    add pricefeed redeployment script

---
+++ scripts/deployment/redeploy-pricefeed.py
@@ -0,0 +1,17 @@
+exec(open("./scripts/env/set-eth.py").read())
+
+
+price_feed = PriceFeeds.deploy({"from": accounts[0]})
+price_feed.changeGuardian(GUARDIAN_MULTISIG, {"from": accounts[0]})
+price_feed.transferOwnership(BZX, {"from": accounts[0]})
+
+tokenList = []
+priceFeedList = []
+for l in list:
+    tokenList.append(l[1])
+    priceFeedList.append(PRICE_FEED.pricesFeeds(l[1]))
+    
+price_feed.setPriceFeed(tokenList, priceFeedList, {"from": GUARDIAN_MULTISIG})
+
+BZX.setPriceFeedContract(price_feed, {"from": BZX.owner()})
+
