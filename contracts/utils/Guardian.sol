5e3c2d1c6a04d3110c3be292cb59d9502f77c34e
RomanH <romeo8881@gmail.com>
2022-07-22 15:45:14 +0300

    possibility to add minimum interest rates

---
+++ contracts/utils/Guardian.sol
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2017-2022, OokiDao. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0.
+ */
+
+pragma solidity >=0.8.4;
+
+import "@openzeppelin-4.3.2/access/Ownable.sol";
+
+
+contract Guardian is Ownable {
+
+    // keccak256("Pausable_FunctionPause")
+    bytes32 internal constant Pausable_FunctionPause = 0xa7143c84d793a15503da6f19bf9119a2dac94448ca45d77c8bf08f57b2e91047;
+
+    // keccak256("Pausable_GuardianAddress")
+    bytes32 internal constant Pausable_GuardianAddress = 0x80e6706973d0c59541550537fd6a33b971efad732635e6c3b99fb01006803cdf;
+
+    modifier pausable {
+        require(!_isPaused(msg.sig), "paused");
+        _;
+    }
+
+    modifier onlyGuardian {
+        require(msg.sender == getGuardian() || msg.sender == owner(), "unauthorized");
+        _;
+    }
+
+    function _isPaused(bytes4 sig) public view returns (bool isPaused) {
+        bytes32 slot = keccak256(abi.encodePacked(sig, Pausable_FunctionPause));
+        assembly {
+            isPaused := sload(slot)
+        }
+    }
+
+    function toggleFunctionPause(bytes4 sig) public onlyGuardian {
+        bytes32 slot = keccak256(abi.encodePacked(sig, Pausable_FunctionPause));
+        assembly {
+            sstore(slot, 1)
+        }
+    }
+
+    function toggleFunctionUnPause(bytes4 sig) public onlyGuardian {
+        // only DAO can unpause, and adding guardian temporarily
+        bytes32 slot = keccak256(abi.encodePacked(sig, Pausable_FunctionPause));
+        assembly {
+            sstore(slot, 0)
+        }
+    }
+
+    function changeGuardian(address newGuardian) public onlyGuardian {
+        assembly {
+            sstore(Pausable_GuardianAddress, newGuardian)
+        }
+    }
+
+    function getGuardian() public view returns (address guardian) {
+        assembly {
+            guardian := sload(Pausable_GuardianAddress)
+        }
+    }
+
+    function pause(bytes4 [] calldata sig)
+        external
+        onlyGuardian
+    {
+        for(uint256 i = 0; i < sig.length; ++i){
+            toggleFunctionPause(sig[i]);
+        }
+    }
+
+    function unpause(bytes4 [] calldata sig)
+        external
+        onlyGuardian
+    {
+        for(uint256 i = 0; i < sig.length; ++i){
+            toggleFunctionUnPause(sig[i]);
+        }
+    }
+}
