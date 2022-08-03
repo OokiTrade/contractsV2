5a463a6320f030e5f2a6863a495954af23db1deb
esvet <shvets.evghenii@gmail.com>
2022-08-03 23:14:48 +0300

    added buyback script (every hour)

---
+++ scripts/p125_buyback.py
@@ -0,0 +1,20 @@
+import schedule
+import time
+
+deployer = accounts.load("0xF6c5B9c0B57590A5be6f16380D68eAC6fd9d0Fac")
+exec(open("./scripts/env/set-matic.py").read())
+buyback = Contract.from_abi("buyback","0x12ebd8263a54751aaf9d8c2c74740a8e62c0afbe",BuyBackAndBurn.abi)
+
+amount = USDC.balanceOf(buyback)
+if(amount == 0):
+    exit(0)
+percent = 100e6 * 1e18/amount * 100
+
+def job():
+    buyback.buyBack(percent, {'from': deployer})
+
+schedule.every().hour.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
