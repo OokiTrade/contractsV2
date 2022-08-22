2e6dc33d826ab564586d133d5f7b3901ae868265
Roman Hiden <romeo8881@gmail.com>
2022-08-22 20:59:05 +0300

    updating lower adming; fix small issue in curved interest

---
+++ scripts/deployment/deploy_curved_interest_rate.py
@@ -0,0 +1,41 @@
+from brownie import *
+
+exec(open("./scripts/env/set-arbitrum.py").read())
+deployer = accounts[0]
+
+MINIMAL_RATES = {
+    # "iETH":   0.01e18,
+    # "iBTC":   0.1e18,
+    # "iSPELL": 0.1e18,
+    # "iLINK":  0.1e18,
+    # "iUSDC":  0.8e18,
+    # "iUSDT":  0.8e18,
+    # "iMIM":   0.1e18,
+    # "iFRAX":  0.8e18,
+    "iETH":   0.1e18,
+    "iBTC":   0.1e18,
+    "iSPELL": 0.1e18,
+    "iLINK":  0.1e18,
+    "iUSDC":  0.1e18,
+    "iUSDT":  0.1e18,
+    "iMIM":   0.1e18,
+    "iFRAX":  0.1e18,
+}
+
+cui = CurvedInterestRate.deploy({'from': deployer})
+cui.updateParams((120e18, 80e18, 100e18, 100e18, 110e18, 0.01e18, 1e11), ZERO_ADDRESS) # default across all
+
+supportedTokenAssetsPairs = TOKEN_REGISTRY.getTokens(0, 100)
+for assetPair in supportedTokenAssetsPairs:
+    existingIToken = Contract.from_abi("existingIToken", address=assetPair[0], abi=interface.IToken.abi)
+    print("itoken", existingIToken.symbol(), assetPair[0])
+    cui.updateParams((120e18, 80e18, 100e18, 100e18, 110e18, MINIMAL_RATES.get(existingIToken.symbol()), 1e11), existingIToken)
+
+
+
+cui.changeGuardian(GUARDIAN_MULTISIG, {"from": deployer})
+cui.transferOwnership(GUARDIAN_MULTISIG, {"from": deployer})
+
+# for assetPair in supportedTokenAssetsPairs:
+#     existingIToken = Contract.from_abi("existingIToken", address=assetPair[0], abi=interface.IToken.abi)
+#     existingIToken.updateSettings(LOAN_TOKEN_SETTINGS_LOWER_ADMIN, LOAN_TOKEN_SETTINGS_LOWER_ADMIN.setDemandCurve.encode_input(cui), {"from": GUARDIAN_MULTISIG})
