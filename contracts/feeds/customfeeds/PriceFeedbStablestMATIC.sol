6e60911a4ffaeac62b57e87e2d3ab245c391b64d
Drypto13 <arenbalian13@gmail.com>
2022-08-23 17:44:27 -0700

    version upgrade

--- contracts/feeds/customfeeds/PriceFeedbStablestMATIC.sol
+++ contracts/feeds/customfeeds/PriceFeedbStablestMATIC.sol
@@ -4,9 +4,9 @@ import "../../../interfaces/IPriceFeeds.sol";
 import "../../interfaces/IBalancerVault.sol";
 import "@openzeppelin-4.7.0/token/ERC20/IERC20.sol";
 contract PriceFeedbStablestMATIC {
-    address internal _vault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
+    address internal constant _vault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
     bytes32 internal constant _poolId = 0x0297e37f1873d2dab4487aa67cd56b58e2f27875000100000000000000000002;
-    IPriceFeeds internal _priceFeed = IPriceFeeds(0x600F8E7B10CF6DA18871Ff79e4A61B13caCEd9BC);
+    IPriceFeeds internal constant _priceFeed = IPriceFeeds(0x600F8E7B10CF6DA18871Ff79e4A61B13caCEd9BC);
 
     address public constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
     address public constant STMATIC = 0x3A58a54C066FdC0f2D55FC9C89F0415C92eBf3C4;
--- contracts/protocoltoken/migrations/MintCoordinator.sol
+++ contracts/protocoltoken/migrations/MintCoordinator.sol
@@ -5,8 +5,8 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin-4.3.2/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin-4.3.2/access/Ownable.sol";
+import "@openzeppelin-4.7.0/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin-4.7.0/access/Ownable.sol";
 import "../OokiToken.sol";
  
 contract MintCoordinator is Ownable {
