ee60c9634e31eea5c9c45795c06f7f4be6c5593d
Roman Hiden <romeo8881@gmail.com>
2022-08-24 16:29:05 +0300

    added deploy script for price_feeds

--- scripts/deployment/deploy_curved_interest_rate.py
+++ scripts/deployment/deploy_curved_interest_rate.py
@@ -22,14 +22,15 @@ MINIMAL_RATES = {
     # "iFRAX":  0.1e18,
 }
 
-cui = CurvedInterestRate.deploy({'from': deployer})
-cui.updateParams((120e18, 80e18, 100e18, 100e18, 110e18, 0.1e18, 0.1e18), ZERO_ADDRESS) # default across all
+# cui = CurvedInterestRate.deploy({'from': deployer})
+cui = CurvedInterestRate.at("0x11e1251651bA36aD58B6bdaCaf11E5347a5D6e50")
+cui.updateParams((120e18, 80e18, 100e18, 100e18, 110e18, 0.1e18, 0.01e18), ZERO_ADDRESS, {"from": deployer}) # default across all
 
 supportedTokenAssetsPairs = TOKEN_REGISTRY.getTokens(0, 100)
 for assetPair in supportedTokenAssetsPairs:
     existingIToken = Contract.from_abi("existingIToken", address=assetPair[0], abi=interface.IToken.abi)
     print("itoken", existingIToken.symbol(), assetPair[0])
-    cui.updateParams((120e18, 80e18, 100e18, 100e18, 110e18, MINIMAL_RATES.get(existingIToken.symbol()), MINIMAL_RATES.get(existingIToken.symbol())), existingIToken)
+    cui.updateParams((120e18, 80e18, 100e18, 100e18, 110e18, MINIMAL_RATES.get(existingIToken.symbol()), MINIMAL_RATES.get(existingIToken.symbol())), existingIToken, {"from": deployer})
 
 
 
