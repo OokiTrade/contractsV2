2f3530f606b444436c2780586eba9b5bde355dec
Drypto13 <arenbalian13@gmail.com>
2022-08-29 15:42:31 -0700

    changing constants names

--- contracts/feeds/customfeeds/PriceFeedbStablestMATIC.sol
+++ contracts/feeds/customfeeds/PriceFeedbStablestMATIC.sol
@@ -6,12 +6,9 @@ import "../../interfaces/IBalancerPool.sol";
 
 import "@openzeppelin-4.7.0/token/ERC20/IERC20.sol";
 contract PriceFeedbStablestMATIC {
-    address internal constant _vault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
-    bytes32 internal constant _poolId = 0xaf5e0b5425de1f5a630a8cb5aa9d97b8141c908d000200000000000000000366;
-    IPriceFeeds internal constant _priceFeed = IPriceFeeds(0x600F8E7B10CF6DA18871Ff79e4A61B13caCEd9BC);
+    IPriceFeeds internal constant _PRICEFEED = IPriceFeeds(0x600F8E7B10CF6DA18871Ff79e4A61B13caCEd9BC);
 
     address public constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
-    address public constant STMATIC = 0x3A58a54C066FdC0f2D55FC9C89F0415C92eBf3C4;
     address public constant USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
     address public constant BSTABLEPOOL = 0xaF5E0B5425dE1F5a630A8cB5AA9D97B8141C908D;
 
@@ -25,6 +22,6 @@ contract PriceFeedbStablestMATIC {
         IBalancerPool.OracleAverageQuery[] memory sets = new IBalancerPool.OracleAverageQuery[](1);
         sets[0] = TWAP;
         uint256 poolPrice = IBalancerPool(BSTABLEPOOL).getTimeWeightedAverage(sets)[0];
-        USDCTotals = int256(_priceFeed.queryReturn(WMATIC, USDC, poolPrice))*100;
+        USDCTotals = int256(_PRICEFEED.queryReturn(WMATIC, USDC, poolPrice))*100;
     }
 }
--- contracts/feeds/customfeeds/PriceFeedbStablestMATICVault.sol
+++ contracts/feeds/customfeeds/PriceFeedbStablestMATICVault.sol
@@ -4,13 +4,13 @@ import "../../../interfaces/IPriceFeeds.sol";
 import "../../interfaces/IVault.sol";
 
 contract PriceFeedbStablestMATICVault {
-    IPriceFeeds internal constant _priceFeed = IPriceFeeds(0x600F8E7B10CF6DA18871Ff79e4A61B13caCEd9BC);
-    IVault internal constant _bStableVault = IVault(0x976f31D12df9272f10c2f20BE2887824Cc3d974c);
+    IPriceFeeds internal constant _PRICEFEED = IPriceFeeds(0x600F8E7B10CF6DA18871Ff79e4A61B13caCEd9BC);
+    IVault internal constant _BSTABLEVAULT = IVault(0x976f31D12df9272f10c2f20BE2887824Cc3d974c);
 
     address public constant BSTABLE = 0xaF5E0B5425dE1F5a630A8cB5AA9D97B8141C908D;
     address public constant USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
 
     function latestAnswer() external view returns (int256) {
-        return int256(_priceFeed.queryReturn(BSTABLE, USDC, _bStableVault.convertToAssets(1e18)))*100;
+        return int256(_PRICEFEED.queryReturn(BSTABLE, USDC, _BSTABLEVAULT.convertToAssets(1e18)))*100;
     }
 }
