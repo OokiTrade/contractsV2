23249e07779bda9332fe9bb797a234a695bedfee
Drypto | OOKI <51417606+Drypto13@users.noreply.github.com>
2022-09-09 02:49:20 -0700

    update due to permission changes

--- scripts/deployment/redeploy-pricefeed.py
+++ scripts/deployment/redeploy-pricefeed.py
@@ -3,7 +3,7 @@ exec(open("./scripts/env/set-arbitrum.py").read())
 
 price_feed = PriceFeeds.deploy({"from": accounts[0]})
 price_feed.changeGuardian(GUARDIAN_MULTISIG, {"from": accounts[0]})
-price_feed.transferOwnership(BZX, {"from": accounts[0]})
+price_feed.transferOwnership(GUARDIAN_MULTISIG, {"from": accounts[0]})
 
 tokenList = []
 priceFeedList = []
@@ -11,7 +11,7 @@ for l in list:
     tokenList.append(l[1])
     priceFeedList.append(PRICE_FEED.pricesFeeds(l[1]))
     
-price_feed.setPriceFeed(tokenList, priceFeedList, {"from": GUARDIAN_MULTISIG})
+price_feed.setPriceFeed(tokenList, priceFeedList, {"from": price_feed.owner()})
 
 BZX.setPriceFeedContract(price_feed, {"from": BZX.owner()})
 
