57d293ac3687ebf4837011e43e578312b655da4b
RomanH <romeo8881@gmail.com>
2022-09-20 19:24:33 +0300

    deployed contracts so far

--- scripts/deployment/deploy-itoken-collateral.py
+++ scripts/deployment/deploy-itoken-collateral.py
@@ -1,11 +1,20 @@
 exec(open("./scripts/deployment/redeploy-pricefeed.py").read())
 deployer = accounts[2]
 
-tickMathV1 = accounts[0].deploy(TickMathV1)
-liquidationHelper = accounts[0].deploy(LiquidationHelper)
-volumeTracker = accounts[0].deploy(VolumeTracker)
+# tickMathV1 = deployer.deploy(TickMathV1)
+# <TickMathV1 Contract '0x210b227Bec2EAb4Fc499Ecfa7347C4Bc969a95d9'>
+tickMathV1 = TickMath.at("0x37A3fC76105c51D54a9c1c3167e30601EdeE8782")
+# liquidationHelper = deployer.deploy(LiquidationHelper)
+# <LiquidationHelper Contract '0x136E7845DC56f31aA80d07C35Cfdc01dAfCCF666'>
+liquidationHelper = LiquidationHelper.at("0x136E7845DC56f31aA80d07C35Cfdc01dAfCCF666")
+# volumeTracker = deployer.deploy(VolumeTracker)
+# <VolumeTracker Contract '0xFd1A56A9c6cD0B5dAef7956Efc131d7A39d4Ab38'>
+volumeTracker = VolumeTracker.at("0xFd1A56A9c6cD0B5dAef7956Efc131d7A39d4Ab38")
 
-lo = deployer.deploy(LoanOpenings)
+# lo = deployer.deploy(LoanOpenings)
+# <LoanOpenings Contract '0xAC87a33dbeD43ca80b8C1e78A685D9ed6cf78eC5'>
+lo = LoanOpenings.at("0xAC87a33dbeD43ca80b8C1e78A685D9ed6cf78eC5")
+
 ls = deployer.deploy(LoanSettings)
 ps = deployer.deploy(ProtocolSettings)
 lcs= deployer.deploy(LoanClosingsLiquidation)
@@ -21,7 +30,7 @@ BZX.replaceContract(lc, {"from": BZX.owner()})
 BZX.replaceContract(lm, {"from": BZX.owner()})
 BZX.replaceContract(se, {"from": BZX.owner()})
 
-helperImpl = HelperImpl.deploy({"from": accounts[0]})
+helperImpl = HelperImpl.deploy({"from": deployer})
 HELPER = Contract.from_abi("HELPER", HELPER, HelperProxy.abi)
 HELPER.replaceImplementation(helperImpl, {"from": GUARDIAN_MULTISIG})
 HELPER = Contract.from_abi("HELPER", HELPER, HelperImpl.abi)
