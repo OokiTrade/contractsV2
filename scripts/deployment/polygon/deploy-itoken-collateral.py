2a799248178cabd70ef7fa7de3d8f0e5a8920f5d
Osama <osama@ooki.com>
2022-11-25 12:50:28 +0200

    preparing for deployment

--- scripts/deployment/polygon/deploy-itoken-collateral.py
+++ scripts/deployment/polygon/deploy-itoken-collateral.py
@@ -27,18 +27,29 @@ lc = LoanClosings.at("0x144eBb90B7A3099e5403a98E03586a5BAc39b9C2")
 lm = LoanMaintenance.at("0xB731075B2837f37BC0d88f707fE0F94e96362495")
 se = SwapsExternal.at("0x695926763fd97E3ef975f21E453e925b4F300A9A")
 
-BZX.replaceContract(lo, {"from": BZX.owner()})
-BZX.replaceContract(ls, {"from": BZX.owner()})
-BZX.replaceContract(ps, {"from": BZX.owner()})
-BZX.replaceContract(lcs,{"from": BZX.owner()})
-BZX.replaceContract(lc, {"from": BZX.owner()})
-BZX.replaceContract(lm, {"from": BZX.owner()})
-BZX.replaceContract(se, {"from": BZX.owner()})
+tx_list = []
+
+# BZX.replaceContract(lo, {"from": BZX.owner()})
+# BZX.replaceContract(ls, {"from": BZX.owner()})
+# BZX.replaceContract(ps, {"from": BZX.owner()})
+# BZX.replaceContract(lcs,{"from": BZX.owner()})
+# BZX.replaceContract(lc, {"from": BZX.owner()})
+# BZX.replaceContract(lm, {"from": BZX.owner()})
+# BZX.replaceContract(se, {"from": BZX.owner()})
+
+tx_list.append([BZX, BZX.replaceContract.encode_input(lo)])
+tx_list.append([BZX, BZX.replaceContract.encode_input(ls)])
+tx_list.append([BZX, BZX.replaceContract.encode_input(ps)])
+tx_list.append([BZX, BZX.replaceContract.encode_input(lcs)])
+tx_list.append([BZX, BZX.replaceContract.encode_input(lc)])
+tx_list.append([BZX, BZX.replaceContract.encode_input(lm)])
+tx_list.append([BZX, BZX.replaceContract.encode_input(se)])
 
 # helperImpl = HelperImpl.deploy({"from": accounts[0]}) # 0xD3297d69F3A08e85977cc855b16E4192C4190bFa
 helperImpl = HelperImpl.at("0xD3297d69F3A08e85977cc855b16E4192C4190bFa")
 HELPER = Contract.from_abi("HELPER", HELPER, HelperProxy.abi)
-HELPER.replaceImplementation(helperImpl, {"from": GUARDIAN_MULTISIG})
+# HELPER.replaceImplementation(helperImpl, {"from": GUARDIAN_MULTISIG})
+tx_list.append([HELPER, HELPER.replaceImplementation.encode_input(helperImpl)])
 HELPER = Contract.from_abi("HELPER", HELPER, HelperImpl.abi)
 
 # itokenImpl = deployer.deploy(LoanTokenLogicStandard) # 0xFb9EA340AE25FF20305d079E5363379F06aCFE4c
@@ -49,23 +60,35 @@ itokenImplWeth = LoanTokenLogicWeth.at("0xf68d5d1A35Db485cA5fEAf830bc81c9d3F2DeE
 for l in list:
     iToken = Contract.from_abi("LoanTokenLogicStandard", address=l[0], abi=interface.IToken.abi)
     if(iToken == iETH):
-        iToken.setTarget(itokenImplWeth, {"from": iToken.owner()})
+        # iToken.setTarget(itokenImplWeth, {"from": iToken.owner()})
+        tx_list.append([iToken, iToken.setTarget.encode_input(itokenImplWeth)])
     else:
-        iToken.setTarget(itokenImpl, {"from": iToken.owner()})
-    iToken.initializeDomainSeparator({"from": iToken.owner()})
-    BZX.migrateLoanParamsList(l[0], 0, 1000, {"from": BZX.owner()})
+        # iToken.setTarget(itokenImpl, {"from": iToken.owner()})
+        tx_list.append([iToken, iToken.setTarget.encode_input(itokenImpl)])
 
+    # iToken.initializeDomainSeparator({"from": iToken.owner()})
+    # BZX.migrateLoanParamsList(l[0], 0, 1000, {"from": BZX.owner()})
+
+    tx_list.append([iToken, iToken.initializeDomainSeparator.encode_input()])
+    tx_list.append([BZX, BZX.migrateLoanParamsList.encode_input(l[0], 0, 1000)])
 
 price_feed_new = PriceFeeds.at("0xDB0f02A68e5b52A853d01c5e1d935645FF5c01D4") # not verified
-BZX.setPriceFeedContract(price_feed_new, {"from": GUARDIAN_MULTISIG})
+# BZX.setPriceFeedContract(price_feed_new, {"from": GUARDIAN_MULTISIG})
+tx_list.append([BZX, BZX.setPriceFeedContract.encode_input(price_feed_new)])
 
-# small test
-USDC.transfer(accounts[0], 100000e6, {"from": "0x0d0707963952f2fba59dd06f2b425ace40b492fe"})
-USDC.approve(iUSDC, 2**256-1, {"from": accounts[0]})
-iUSDC.mint(accounts[0], 10000e6, {"from": accounts[0]})
 
-iTokens = [item[0] for item in TOKEN_REGISTRY.getTokens(0, 100)]
-BZX.setSupportedTokens(iTokens, [True] * len(iTokens), True, {'from': GUARDIAN_MULTISIG})
+# for tx in tx_list:
+#     sTxn = safe.build_multisig_tx(tx[0].address, 0, tx[1], SafeOperation.CALL.value, safe_nonce=safe.pending_nonce())
+#     safe.sign_with_frame(sTxn)
+#     safe.post_transaction(sTxn)
 
-iUSDC.approve(iUSDT, 2**256-1, {"from": accounts[0]})
-iUSDT.borrow("", 50e6, 0, 100e6, iUSDC, accounts[0], accounts[0], b"", {'from': accounts[0]})
+# # small test
+# USDC.transfer(accounts[0], 100000e6, {"from": "0x0d0707963952f2fba59dd06f2b425ace40b492fe"})
+# USDC.approve(iUSDC, 2**256-1, {"from": accounts[0]})
+# iUSDC.mint(accounts[0], 10000e6, {"from": accounts[0]})
+
+# iTokens = [item[0] for item in TOKEN_REGISTRY.getTokens(0, 100)]
+# BZX.setSupportedTokens(iTokens, [True] * len(iTokens), True, {'from': GUARDIAN_MULTISIG})
+
+# iUSDC.approve(iUSDT, 2**256-1, {"from": accounts[0]})
+# iUSDT.borrow("", 50e6, 0, 100e6, iUSDC, accounts[0], accounts[0], b"", {'from': accounts[0]})
