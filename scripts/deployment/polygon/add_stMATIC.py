e4e1d5816af66ade2102d6599fb991a5313729ff
Drypto13 <arenbalian13@gmail.com>
2022-12-12 01:04:24 -0800

    Create add_stMATIC.py

---
+++ scripts/deployment/polygon/add_stMATIC.py
@@ -0,0 +1,25 @@
+from brownie import *
+
+exec(open("./scripts/env/set-matic.py").read())
+
+STMATIC_FEED = Contract.from_abi("","0xF47a71F71B2b9A85c8d49c747e5C002e77245302",PriceFeedstMATIC.abi)
+STMATIC = "0x9ee91F9f426fA633d227f7a9b000E28b9dfd8599"
+MATIC = "0x7c9f4C87d911613Fe9ca58b579f737911AAD2D43"
+NULL = "0x0000000000000000000000000000000000000000"
+calls = []
+tokens = []
+for l in list:
+    tokens.append(l[1])
+calls.append([PRICE_FEED.address,False,PRICE_FEED.setPriceFeed.encode_input([STMATIC],[STMATIC_FEED.address])])
+#add balancer swap impl to dex records
+calls.append([BZX.swapsImpl(),False,Contract.from_abi("",BZX.swapsImpl(),DexRecords.abi).setDexID.encode_input("0x5281d5BbA9162B41320A2fc760F67B9e02fFd793")])
+#set the tokens to be supported
+calls.append([BZX.address,False,BZX.setSupportedTokens.encode_input([STMATIC],[True], True)])
+#calls.append([BZX.address,False,BZX.setApprovals.encode_input(tokens,[Contract.from_abi("",BZX.swapsImpl(),DexRecords.abi).getDexCount()])])
+#modify params for stMATIC/MATIC to be 10% initial and 7% maintenance
+STMATIC_PARAMS = [BZX.generateLoanParamId(MATIC, STMATIC,True),True,NULL,MATIC,STMATIC,10e18,7e18,0]
+STMATIC_PARAM = [BZX.generateLoanParamId(MATIC, STMATIC,False),True,NULL,MATIC,STMATIC,10e18,7e18,1]
+
+calls.append([BZX.address,False,BZX.modifyLoanParams.encode_input([STMATIC_PARAMS,STMATIC_PARAM])])
+
+print(interface.IMulticall3("0xcA11bde05977b3631167028862bE2a173976CA11").aggregate3.encode_input(calls))
