a090a60aba6c2b09abd48617e3c8f5a2101dcf64
Osama <osama@ooki.com>
2023-03-16 22:39:40 +0200

    improved stMATIC/MATIC test

--- contracts/feeds/customfeeds/PriceFeedstMATICImproved.sol
+++ contracts/feeds/customfeeds/PriceFeedstMATICImproved.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "../../interfaces/IstMATICRateProvider.sol";
 import "../../../interfaces/IPriceFeeds.sol";
 import "../IPriceFeedsExt.sol";
-contract PriceFeedstMATIC {
+contract PriceFeedstMATICImproved {
 
     IPriceFeedsExt public constant MATIC_PRICE_FEED = IPriceFeedsExt(0x5d37E4b374E6907de8Fc7fb33EE3b0af403C7403);
     address internal constant _MATICRATEPROVIDER = 0xdEd6C522d803E35f65318a9a4d7333a22d582199;
--- testspolygon/balancer/test_balancer_change.py
+++ testspolygon/balancer/test_balancer_change.py
@@ -23,6 +23,15 @@ def USDC(interface):
 def PRICE_FEED(Contract, BZX, PriceFeeds):
     return Contract.from_abi("PRICE_FEED", BZX.priceFeeds(), PriceFeeds.abi)
 
+@pytest.fixture(scope="module")
+def iETH(accounts, interface, LoanTokenLogicWeth):
+    itoken = Contract.from_abi("iETH", address="0xB8329B5458B1E493EFd8D9DA8C3B5E6D68e67C21", abi=interface.IToken.abi)
+    return itoken
+
+@pytest.fixture(scope="module")
+def iMATIC(accounts, interface, LoanTokenLogicWeth):
+    itoken = Contract.from_abi("iETH", address="0x81B91c9a68b94F88f3DFC4F375f101223dDd5007", abi=interface.IToken.abi)
+    return itoken
 
 def test_swaps(Contract, SwapsImplBalancer_POLYGON, DexRecords, BZX, stMATIC, WMATIC, USDC):
 
@@ -63,52 +72,78 @@ def test_swaps(Contract, SwapsImplBalancer_POLYGON, DexRecords, BZX, stMATIC, WM
         BZX.swapExternal(stMATIC, WMATIC, accounts[0], accounts[0], 5e17, 5e17, loanDataBytes, {"from":accounts[0]}).return_value
 
 #test case 2. trade test case where it uses Balancer to open and close
-def test_case2(Contract, SwapsImplBalancer_POLYGON, DexRecords, BZX, stMATIC, WMATIC, USDC, PRICE_FEED):
+def test_case2(Contract, SwapsImplBalancer_POLYGON, DexRecords, BZX, stMATIC, WMATIC, USDC, PRICE_FEED, iMATIC):
     
-    bal = SwapsImplBalancer_POLYGON.deploy({"from":accounts[0]})
+    STMATIC_SWAP_IMPL = SwapsImplBalancer_POLYGON.deploy({"from":accounts[0]})
     STMATIC_SWAP_IMPL_ID = 4
     DEX_RECORDS = Contract.from_abi("DEX_RECORDS",BZX.swapsImpl(),DexRecords.abi)
-    DEX_RECORDS.setDexID(WSTETH_SWAP_IMPL_ID, WSTETH_SWAP_IMPL, {'from':DEX_RECORDS.owner()})
-    PRICE_FEED.setPriceFeed([WSTETH], [WSTETH_PRICE_FEED], {'from': PRICE_FEED.owner()})
-    BZX.setSupportedTokens([WETH, WSTETH], [True, True], True, {'from': BZX.owner()})
-    BZX.modifyLoanParams([[BZX.generateLoanParamId(WETH,WSTETH,False),True,iETH,WETH,WSTETH,int(6.6667*1e18), int(5*1e18), 1]],{'from':BZX.owner()}) #set params for margin trading
+    DEX_RECORDS.setDexID(STMATIC_SWAP_IMPL_ID, STMATIC_SWAP_IMPL, {'from':DEX_RECORDS.owner()})
+    # PRICE_FEED.setPriceFeed([stMATIC], [WSTETH_PRICE_FEED], {'from': PRICE_FEED.owner()})
+    # BZX.setSupportedTokens([WETH, WSTETH], [True, True], True, {'from': BZX.owner()})
+    # BZX.modifyLoanParams([[BZX.generateLoanParamId(WETH,WSTETH,False),True,iETH,WETH,WSTETH,int(6.6667*1e18), int(5*1e18), 1]],{'from':BZX.owner()}) #set params for margin trading
     
-    amountOfEthToTrade = 1*10**18
+    amountOfMATICToTrade = 1*10**18
     FLAGS_DEX_SELECTOR_FLAG = 2
-    LEAVERAGE = 15
+    LEAVERAGE = 4
     FEE = 0.1
     
-    # minAmountOfStEthToReceive = WSTETH_SWAP_IMPL.dexAmountOutFormatted.call(encode_abi(['uint256',"address", "address"],[amountOfEthToTrade * LEAVERAGE, WETH.address, WSTETH.address]), amountOfEthToTrade * LEAVERAGE)
-    dex_payload = encode_abi(['uint256',"address", "address"],[amountOfEthToTrade * LEAVERAGE, WETH.address, WSTETH.address])
-    selector_payload = encode_abi(['uint256','bytes'],[WSTETH_SWAP_IMPL_ID, dex_payload]) # 
+    # minAmountOfStEthToReceive = WSTETH_SWAP_IMPL.dexAmountOutFormatted.call(encode_abi(['uint256',"address", "address"],[amountOfMATICToTrade * LEAVERAGE, WETH.address, WSTETH.address]), amountOfMATICToTrade * LEAVERAGE)
+    from enum import Enum
+    # in every pool assets have a fixed index. index for an asset is changed from pool to pool
+    class Assets(Enum):
+        WMATIC = 0
+        stMATIC = 1
+        bstMATIC = 2
+    
+    poolId = bytes.fromhex("8159462d255c1d24915cb51ec361f700174cd99400000000000000000000075d")
+    assetInIndex = Assets.WMATIC.value
+    assetOutIndex = Assets.stMATIC.value
+    amount = amountOfMATICToTrade
+    userData = b''
+    batchSwapStep = [(poolId, assetInIndex, assetOutIndex, amount, userData)]
+    swapPath = [WMATIC.address, stMATIC.address]
+    limits = [0, 0]
+    dex_payload = encode_abi(['(bytes32,uint256,uint256,uint256,bytes)[]','address[]','uint256[]'],[batchSwapStep, swapPath, limits])
+    selector_payload = encode_abi(['uint256','bytes'],[STMATIC_SWAP_IMPL_ID, dex_payload])
     loanDataBytes = encode_abi(['uint128','bytes[]'],[FLAGS_DEX_SELECTOR_FLAG, [selector_payload]]) #flag value of Base-2: 10, -> Flags_DEX_SELECTOR_FLAG
-    minAmountOfStEthToReceive = BZX.getSwapExpectedReturn.call(accounts[0], WETH.address, WSTETH.address, amountOfEthToTrade * LEAVERAGE, loanDataBytes, True, {"from": accounts[0]})
+
+    minAmountOfStEthToReceive = BZX.getSwapExpectedReturn.call(accounts[0], WMATIC.address, stMATIC.address, amountOfMATICToTrade * LEAVERAGE, loanDataBytes, True, {"from": accounts[0]})
 
 
     #prep data
-    dex_payload = encode_abi(['uint256'],[minAmountOfStEthToReceive])
-    selector_payload = encode_abi(['uint256','bytes'],[WSTETH_SWAP_IMPL_ID, dex_payload]) # 
-    loanDataBytes = encode_abi(['uint128','bytes[]'],[FLAGS_DEX_SELECTOR_FLAG, [selector_payload]]) #flag value of Base-2: 10, -> Flags_DEX_SELECTOR_FLAG
 
-    balanceBefore = WSTETH.balanceOf(BZX)
+
+    balanceBefore = stMATIC.balanceOf(BZX)
     #margin trade
-    iETH.marginTrade(0, (LEAVERAGE-1)*1e18, amountOfEthToTrade, 0, WSTETH,accounts[0],loanDataBytes,{'from':accounts[0],'value':amountOfEthToTrade}) #15x position
+    iMATIC.marginTrade(0, (LEAVERAGE-1)*1e18, amountOfMATICToTrade, 0, stMATIC,accounts[0],loanDataBytes,{'from':accounts[0],'value':amountOfMATICToTrade}) #15x position
 
     loanToAnalyze = BZX.getUserLoans(accounts[0],0,10,0,False,False)[0]
 
-    assert(loanToAnalyze[4] == 14e18)
-    assert(loanToAnalyze[5] >= 13e18)
-    assert(WSTETH.balanceOf(BZX)-balanceBefore==loanToAnalyze[5])
+    assert(loanToAnalyze[4] == (LEAVERAGE-1)*1e18)
+    assert(loanToAnalyze[5] == minAmountOfStEthToReceive)
+    assert(stMATIC.balanceOf(BZX)-balanceBefore==loanToAnalyze[5])
 
-    dex_payload = encode_abi(['uint256'],[loanToAnalyze[4]])
-    selector_payload = encode_abi(['uint256','bytes'],[3,dex_payload])
-    loanDataBytes = encode_abi(['uint128','bytes[]'],[2,[selector_payload]]) #flag value of Base-2: 10    
+
+
+    poolId = bytes.fromhex("8159462d255c1d24915cb51ec361f700174cd99400000000000000000000075d")
+    assetInIndex = Assets.stMATIC.value
+    assetOutIndex = Assets.WMATIC.value
+    amount = loanToAnalyze[5]
+    userData = b''
+    batchSwapStep = [(poolId, assetInIndex, assetOutIndex, amount, userData)]
+    swapPath = [stMATIC.address, WMATIC.address]
+    limits = [0, 0]
+    dex_payload = encode_abi(['(bytes32,uint256,uint256,uint256,bytes)[]','address[]','uint256[]'],[batchSwapStep, swapPath, limits])
+    selector_payload = encode_abi(['uint256','bytes'],[STMATIC_SWAP_IMPL_ID, dex_payload])
+    loanDataBytes = encode_abi(['uint128','bytes[]'],[FLAGS_DEX_SELECTOR_FLAG, [selector_payload]]) #flag value of Base-2: 10, -> Flags_DEX_SELECTOR_FLAG
+
+
     BZX.closeWithSwap(loanToAnalyze[0], accounts[0], loanToAnalyze[5], False, loanDataBytes, {'from':accounts[0]})
 
     # assert(BZX.getUserLoans(accounts[0],0,10,0,False,False)[0][0] != loanToAnalyze[0]) # loan already closed
     assert(BZX.getUserLoans(accounts[0],0,10,0,False,False) == ())
-    print(WSTETH.balanceOf(BZX))
-    print(WSTETH.balanceOf(BZX)-loanToAnalyze[5]*0.0015)
-    assert(WSTETH.balanceOf(BZX)-loanToAnalyze[5]*0.0015-balanceBefore <= 10)
-    assert(WSTETH.balanceOf(BZX)-loanToAnalyze[5]*0.0015-balanceBefore >= -10)
+    print(stMATIC.balanceOf(BZX))
+    print(stMATIC.balanceOf(BZX)-loanToAnalyze[5]*0.0015)
+    assert(stMATIC.balanceOf(BZX)-loanToAnalyze[5]*0.0015-balanceBefore <= 10)
+    assert(stMATIC.balanceOf(BZX)-loanToAnalyze[5]*0.0015-balanceBefore >= -10)
     assert False
