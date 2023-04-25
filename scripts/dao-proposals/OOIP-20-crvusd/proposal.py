d8fa87a5658eb0450cfa02bd016976686cfeba7b
yan <yangen@ooki.com>
2023-04-25 20:39:23 +0300

    ooip-20

--- scripts/dao-proposals/OOIP-20-crvusd/proposal.py
+++ scripts/dao-proposals/OOIP-20-crvusd/proposal.py
@@ -2,16 +2,38 @@ from brownie import *
 
 exec(open("./scripts/env/set-eth.py").read())
 
-description = "OOIP-19-pricefeed-update"
+description = "OOIP-20-crvusd"
 
 targets = []
 values = []
 calldatas = []
 
-targets.append(PRICE_FEED.address)
-calldatas.append( PRICE_FEED.setPriceFeed.encode_input([USDC, USDT], ['0x986b5E1e1755e3C2440e960477f25201B0a8bbD4', '0xEe9F2375b4bdF6387aa8265dD4FB8F16512A1d46']))
-values = [0] * len(targets)  # empty array
 
+exec(open("./scripts/env/set-eth.py").read())
+MINIMAL_RATES = {
+    "icrvUSD":   0.1e18
+}
+
+loanTokenLogicStandard = Contract.from_abi("loanTokenLogicStandard", address="0x624f7f89414011b276c60ea2337bfba936d1cbbe", abi=LoanTokenLogicStandard.abi)
+loanTokenAddress = "TBU"
+
+priceFeedAddress = "TBU"
+PRICE_FEED.setPriceFeed([loanTokenAddress], [priceFeedAddress], {"from": TIMELOCK})
+PRICE_FEED.setDecimals([loanTokenAddress], {"from": TIMELOCK})
+
+iToken = Contract.from_abi("iToken", "TBU", LoanTokenLogicStandard.abi)
+
+CUI.updateParams((120e18, 80e18, 100e18, 100e18, 110e18, MINIMAL_RATES.get(iToken.symbol()), MINIMAL_RATES.get(iToken.symbol())), iToken, {"from": TIMELOCK})
+iToken.setDemandCurve(CUI,{"from": deployer})
+
+BZX.setApprovals([loanTokenAddress], [1,2], {'from': TIMELOCK})
+BZX.setupLoanPoolTWAI(iProxy, {"from": TIMELOCK})
+
+BZX.setLoanPool([iToken], [loanTokenAddress], {"from": TIMELOCK})
+BZX.setSupportedTokens([loanTokenAddress, iToken], [True, True], True, {"from": TIMELOCK})
+
+
+values = [0] * len(targets)  # empty array
 signatures = [""] * len(targets)  # empty signatures array
 
 # Make proposal
