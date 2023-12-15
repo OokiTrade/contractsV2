e9f7a958b4066cfd623a0dba9c934d946bd1a329
Osama <osama@ooki.com>
2023-12-15 15:42:22 +0200

    removed empty lines

--- contracts/modules/LoanClosings/LoanClosings.sol
+++ contracts/modules/LoanClosings/LoanClosings.sol
@@ -128,8 +128,7 @@ contract LoanClosings is LoanClosingsShared {
                 loanLocal.lender,
                 loanCloseAmount
             );
-        }
-        
+        }       
 
         if (loanCloseAmount == principalPlusInterest) {
             // collateral is only withdrawn if the loan is closed in full
@@ -218,7 +217,6 @@ contract LoanClosings is LoanClosingsShared {
                 loanCloseAmount
             );
         }
-        
         
         if (usedCollateral != 0) {
             loanLocal.collateral = loanLocal.collateral
