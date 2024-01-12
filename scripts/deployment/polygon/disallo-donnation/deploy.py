38e0ff65d991ed352f2e2aecf694f78274e88335
Osama <osama@ooki.com>
2024-01-12 20:50:10 +0200

    deployment polygon again

--- scripts/deployment/polygon/disallo-donnation/deploy.py
+++ scripts/deployment/polygon/disallo-donnation/deploy.py
@@ -1,7 +1,7 @@
 from brownie import *
 exec(open("./scripts/env/set-matic.py").read())
 deployer = accounts.at("0x70FC4dFc27f243789d07134Be3CA31306fD2C6B6")
-price_feed = Contract.from_abi("PriceFeeds", address="0xCe57167214F969dB55190aac3D0D4732c2Ba04b8", abi=PriceFeeds.abi)
+price_feed = Contract.from_abi("PriceFeeds", address="0x7FCea20C7136649FeFDE497eb3a207e7f401A4B8", abi=PriceFeeds.abi)
 # price_feed = PriceFeeds.deploy({"from": deployer})
 # tokenList = []
 # priceFeedList = []
@@ -13,9 +13,9 @@ price_feed = Contract.from_abi("PriceFeeds", address="0xCe57167214F969dB55190aac
 # price_feed.setPriceFeed(tokenList, priceFeedList, {"from": deployer})
 # price_feed.setDecimals(tokenList, {'from': deployer})
 
-itokenImpl = Contract.from_abi("LoanTokenLogicStandard", address="0x94829cf6B60EfB844762f4eF2da0EdC937B7E2D3", abi=LoanTokenLogicStandard.abi)
+itokenImpl = Contract.from_abi("LoanTokenLogicStandard", address="0x528D205b9521a5e5193b3da9be940c6A18D32a4d", abi=LoanTokenLogicStandard.abi)
 # itokenImpl = LoanTokenLogicStandard.deploy({"from": deployer});
-itokenImplWeth = Contract.from_abi("LoanTokenLogicWeth", address="0x46cdCC87fa0C65364F8dbB084201b8B0b56C2Cf9", abi=LoanTokenLogicWeth.abi)
+itokenImplWeth = Contract.from_abi("LoanTokenLogicWeth", address="0x9B4dC5A53331c8d300Ed75D84525D824D38c028avo", abi=LoanTokenLogicWeth.abi)
 #itokenImplWeth = LoanTokenLogicWeth.deploy({"from": deployer});
 
 # tickMathV1 = Contract.from_abi("TickMathV1", address="0x6796A1F9682a1Cf4A83D237A57A8F9eBEEC6e4F2", abi=TickMathV1.abi)
@@ -28,7 +28,7 @@ volumeTracker = VolumeTracker.at("0x184E9c813EcDE3a55CD3E6aD112aF13Bd00EffD6")
 liquidationHelper = LiquidationHelper.at("0x66Eda677a5E5C48ae0c9a86B1F9Fc1aef2BD8ae6")
 # liquidationHelper = LiquidationHelper.deploy({"from": accounts[0]})
 
-lcl = Contract.from_abi("LoanClosingsLiquidation", address="0x0D2caD590e0C2bEb141aC872aFd94fE17bEc3bFb", abi=LoanClosingsLiquidation.abi) #Unverifyed
+lcl = Contract.from_abi("LoanClosingsLiquidation", address="0xd219325Cf1c4FA17E5984feA5911d0Ba0CaE60F9", abi=LoanClosingsLiquidation.abi) #Unverifyed
 # lcl= LoanClosingsLiquidation.deploy({"from": deployer})
 
 lc = Contract.from_abi("LoanClosings", address="0xd3b1b8E5F0e58f87Fe61Ca5EDBfF8767BB194c82", abi=LoanClosings.abi)
