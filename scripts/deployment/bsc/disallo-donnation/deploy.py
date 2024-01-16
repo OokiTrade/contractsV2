90a896084a0217c868e868556285772c7e4efedf
yan <yangen@ooki.com>
2024-01-16 18:56:47 +0200

    added bsc and optimism

--- scripts/deployment/bsc/disallo-donnation/deploy.py
+++ scripts/deployment/bsc/disallo-donnation/deploy.py
@@ -1,7 +1,7 @@
 from brownie import *
-exec(open("./scripts/env/set-arbitrum.py").read())
-deployer = "0x70FC4dFc27f243789d07134Be3CA31306fD2C6B6"
-price_feed = Contract.from_abi("PriceFeeds", address="0x144eBb90B7A3099e5403a98E03586a5BAc39b9C2", abi=PriceFeeds.abi)
+exec(open("./scripts/env/set-bsc.py").read())
+deployer = accounts[0]
+price_feed = Contract.from_abi("PriceFeeds", address="0x80faA8e034229CF2Cf66036a0e55bc379A62e1ef", abi=PriceFeeds.abi)
 # price_feed = PriceFeeds.deploy({"from": deployer})
 # tokenList = []
 # priceFeedList = []
@@ -13,22 +13,24 @@ price_feed = Contract.from_abi("PriceFeeds", address="0x144eBb90B7A3099e5403a98E
 # price_feed.setPriceFeed(tokenList, priceFeedList, {"from": deployer})
 # price_feed.setDecimals(tokenList, {'from': deployer})
 
-itokenImpl = Contract.from_abi("LoanTokenLogicStandard", address="0xD3297d69F3A08e85977cc855b16E4192C4190bFa", abi=LoanTokenLogicStandard.abi)
-# itokenImpl = LoanTokenLogicStandard.deploy({"from": deployer})
-itokenImplWeth = Contract.from_abi("LoanTokenLogicWeth", address="0xFb9EA340AE25FF20305d079E5363379F06aCFE4c", abi=LoanTokenLogicWeth.abi)
-# itokenImplWeth = LoanTokenLogicWeth.deploy({"from": deployer})
 
-tickMathV1 = TickMathV1.at("0xf68d5d1A35Db485cA5fEAf830bc81c9d3F2DeE0d")
+itokenImpl = Contract.from_abi("LoanTokenLogicStandard", address="0xFB3Cf398624b465d54eb11Eef675f74a53671fb4", abi=LoanTokenLogicStandard.abi)
+# itokenImpl = LoanTokenLogicStandard.deploy({"from": deployer})
+itokenImplWeth = Contract.from_abi("LoanTokenLogicWeth", address="0xc8a86a313C9754C149e2426637E674F8fFbB9348", abi=LoanTokenLogicWeth.abi)
+# itokenImplWeth = LoanTokenLogicWeth.deploy({"from": deployer})
+assert False
+
+tickMathV1 = TickMathV1.at("0xE3c1A9638855ACBe2D2a9f01706d10e9Bc07173b")
 # tickMathV1 = TickMathV1.deploy({"from": deployer})
-volumeTracker = VolumeTracker.at("0x65fC5dc47407318362b6Aef1CBCC777646E5b3E5")
+volumeTracker = VolumeTracker.at("0x316c1DC88aC4dC29F40Bf6AfD0E13eb62F0B9432")
 # volumeTracker = VolumeTracker.deploy({"from": deployer})
-liquidationHelper = LiquidationHelper.at("0x363B3b74cCC01c3ce3C2c63d8BF66ca2BDC2fAE2")
+liquidationHelper = LiquidationHelper.at("0xd4b0014631cE1E2381032AeF5A4821cDFF4067D7")
 # liquidationHelper = LiquidationHelper.deploy({"from": deployer})
 
-lcl = Contract.from_abi("LoanClosingsLiquidation", address="0x5dB579823aF0a2998Fb197e950E9403e9E25ee34", abi=LoanClosingsLiquidation.abi) #Unverifyed
+lcl = Contract.from_abi("LoanClosingsLiquidation", address="0x4a474820AE244E51855156ee6F193d317AF7806e", abi=LoanClosingsLiquidation.abi) #Unverifyed
 # lcl= LoanClosingsLiquidation.deploy({"from": deployer})
 
-lc = Contract.from_abi("LoanClosings", address="0x8F78e94a4f4c92Ef3832C0158e66779777cD819F", abi=LoanClosings.abi)
+lc = Contract.from_abi("LoanClosings", address="0x73422B8677E0Fe5595A4069143f646623b9E38Cb", abi=LoanClosings.abi)
 # lc = LoanClosings.deploy({"from": deployer})
 # price_feed.changeGuardian(GUARDIAN_MULTISIG, {"from": deployer})
 # price_feed.transferOwnership(GUARDIAN_MULTISIG, {"from": deployer})
